83bd440f0ef39bc5aad7b33c8ce194fce0ff7176
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-11-01 17:14:31 +0530

    Updating labautomation

--- setup.sh
+++ setup.sh
@@ -11,11 +11,35 @@ Select an Option > "
 read option
 
 if [ $option -lt 1 -o $option -gt 5 ]; then 
-  echo "Invalis Option. Try Again " 
+  echo "Invalid Option. Try Again " 
   exit 1
 fi
 
-## Check SELINUX adn FIREWALL enabled or not.
+## Importing required source files.
+curl -s https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh >/tmp/common-functions.sh
+source /tmp/common-functions
+## Supported OS's
+# CentOS 7
+if [ ! -f /etc/centos-release ]; then 
+	error "Curently this setup works only for CentOS Operating System"
+	exit 1
+fi
+
+EL=$(rpm -q basesystem | sed -e 's/\./ /g' |xargs -n1 | grep el)
+if [ "$EL" = "el7" ]; then 
+	error "Currently this setup works only for CentOS-7 OS"
+	exit 1
+fi
+
+SESTATUS=$(sestatus|awk '{print $NF}')
+if [ "$SESTATUS" = 'enabled' ]; then 
+	hint "SELINUX and FIREWALL enabled on Server.. Proceeding to disable them and it will  reboot the server"
+	curl -s https://raw.githubusercontent.com/linuxautomations/scripts/master/init.sh | bash
+fi
+
+
+
+## Check SELINUX and FIREWALL enabled or not.
 
 ### Install ansible
 
