c682dd624a8da344edee7321b4d23bc09003a043
linuxautomations <raghukx@gmail.com>
2017-11-01 14:44:10 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,18 @@ echo -n -e "
 2) Apache Tomcat
 3) Apache Tomcat
 4) Application Stack (WEB + APP + DB)
+5) Exit 
 
 Select an Option > "
 read option
+
+if [ $option -lt 1 -o $option -gt 5 ]; then 
+  echo "Invalis Option. Try Again " 
+  exit 1
+fi
+
+## Check SELINUX adn FIREWALL enabled or not.
+
+### Install ansible
+
+### Run Playbook
