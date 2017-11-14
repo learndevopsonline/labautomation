4f99636e090cb569138e72ead28e8c3acc829d6a
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-11-14 23:24:46 +0530

    Updating playbooks

--- setup.sh
+++ setup.sh
@@ -12,6 +12,19 @@ CheckSELinux
 ## Checking Firewall on the Server.
 CheckFirewall
 
+## Supported OS's
+# CentOS 7
+if [ ! -f /etc/centos-release ]; then 
+	error "Curently this setup works only for CentOS Operating System"
+	exit 1
+fi
+
+EL=$(rpm -q basesystem | sed -e 's/\./ /g' |xargs -n1 | grep el)
+if [ "$EL" != "el7" ]; then 
+	error "Currently this setup works only for CentOS-7 OS"
+	exit 1
+fi
+
 echo -n -e "
 1) Apache Web Server
 2) Apache Tomcat
@@ -32,7 +45,7 @@ read option
 C=$(echo $option | cut -c 1)
 [ "$C" -eq 1 ] &>/dev/null
 STAT=$?
-if [ $STAT -eq 2 ]
+if [ $STAT -eq 2 ]; then 
 	if [ "$option" = E ]; then 
 		exit
 	else
@@ -44,30 +57,6 @@ else
 	  error "Invalid Option. Try Again " 
 	  exit 1
 	fi
-fi
-
-
-## Importing required source files.
-curl -s https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh >/tmp/common-functions.sh
-source /tmp/common-functions.sh
-## Supported OS's
-# CentOS 7
-if [ ! -f /etc/centos-release ]; then 
-	error "Curently this setup works only for CentOS Operating System"
-	exit 1
-fi
-
-EL=$(rpm -q basesystem | sed -e 's/\./ /g' |xargs -n1 | grep el)
-if [ "$EL" != "el7" ]; then 
-	error "Currently this setup works only for CentOS-7 OS"
-	exit 1
-fi
-
-## Check SELINUX and FIREWALL enabled or not.
-SESTATUS=$(sestatus|head -1 |awk '{print $NF}')
-if [ "$SESTATUS" = 'enabled' ]; then 
-	hint "SELINUX and FIREWALL enabled on Server.. Proceeding to disable them and it will  reboot the server"
-	curl -s https://raw.githubusercontent.com/linuxautomations/scripts/master/init.sh | bash
 fi
 
 ### Install ansible
