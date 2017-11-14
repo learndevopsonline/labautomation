104cae4f8e2b9f5975ba49d151f5404c0fc61157
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-11-14 22:54:43 +0530

    Updating labautomation

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget -O setup.sh https://raw.githubusercontent.com/linuxautomations/labautomation/master/setup.sh &>/dev/null
-sudo sh setup.sh
+bash <(curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/setup.sh)
