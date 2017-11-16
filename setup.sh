cc05eb8233ebbbdaa5e88a9518d015b76649b996
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-11-16 20:09:07 +0530

    Updating labautomation

--- setup.sh
+++ setup.sh
@@ -60,12 +60,12 @@ else
 fi
 
 ### Install ansible
-if [ ! -x `which ansible` ]; then 
+if [ ! -x `which ansible 2>/dev/null` ]; then 
 	yum install ansible -y &>/dev/null 
 fi
 
 ### Run Playbook
-if [ ! -x `which ansible-playbook` ]; then 
+if [ ! -x `which ansible-playbook 2>/dev/null` ]; then 
 	error "Unable to find ansible-playbook command .. Seems ansible installation didn't went will. Try to install and try again"
 	exit 1
 fi
