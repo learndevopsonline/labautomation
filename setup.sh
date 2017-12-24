e19aa67e68555a9ea43e433f77d5a7c5ec5c10af
linuxautomations <raghukx@gmail.com>
2017-12-24 19:30:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
-#source /root/scripts/common-functions.sh
-source /tmp/common-functions.sh
-## Checking Root User or not.
-CheckRoot
-
-## Checking SELINUX Enabled or not.
-CheckSELinux
-
-## Checking Firewall on the Server.
-CheckFirewall &>/dev/null
-
-## Supported OS's
-# CentOS 7
-if [ ! -f /etc/centos-release ]; then 
-	error "Curently this setup works only for CentOS Operating System"
-	exit 1
-fi
-
-EL=$(rpm -q basesystem | sed -e 's/\./ /g' |xargs -n1 | grep el)
-if [ "$EL" != "el7" ]; then 
-	error "Currently this setup works only for CentOS-7 OS"
-	exit 1
-fi
-
 option=$1
 if [ -z "$option" ]; then 
 
