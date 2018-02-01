95cf253753e6cfcf78985576cc0d521270a3341a
Kalluru <sk185505@ncr.com>
2018-02-01 15:35:20 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -5,6 +5,6 @@ import os
 
 REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm'
 
-out=os.system("rpm -qa | grep -w packagename")
+out=os.system("rpm -qa | grep -w kernel")
 print out
 
