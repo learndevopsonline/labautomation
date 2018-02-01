2fa392db38c06562bfd1cabd046de8500f65aa8c
Kalluru <sk185505@ncr.com>
2018-02-01 16:18:06 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -7,7 +7,7 @@ from termcolor import colored
 REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm'
 
 print colored('hello', 'red'), colored('world', 'green')
-print "Nope, that is not a two. That is a", x
+print "Nope, that is not a two. That is a",
 
 out=os.system("rpm -qa | grep -w zabbix-release &>/dev/null")
 if out != 0:
