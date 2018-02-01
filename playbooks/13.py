73a21908e063ef967d9b6744b77f449a7fc26b4f
Kalluru <sk185505@ncr.com>
2018-02-01 15:49:05 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -2,11 +2,15 @@
 
 #print 'Hello World'
 import os
+import colored
 
 REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm'
 
+print colored('hello', 'red'), colored('world', 'green')
+
 out=os.system("rpm -qa | grep -w zabbix-release &>/dev/null")
 if out != 0:
+    print ""
     OSA=os.system("rpm -ivh "+ REPO_URL + " &>/dev/null")
     #print OSA
 
