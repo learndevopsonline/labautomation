482657e3e3d8ab3bd3731e9773e5cdfec2ac4223
Kalluru <sk185505@ncr.com>
2018-02-01 15:47:19 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -7,6 +7,6 @@ REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.e
 
 out=os.system("rpm -qa | grep -w zabbix-release &>/dev/null")
 if out != 0:
-    OSA=os.system("rpm -ivh "+ REPO_URL)
+    OSA=os.system("rpm -ivh "+ REPO_URL + " &>/dev/null")
     #print OSA
 
