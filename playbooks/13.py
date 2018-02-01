63fd7509b763af342768911265be12e2358b75c8
Kalluru <sk185505@ncr.com>
2018-02-01 16:26:09 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -11,11 +11,14 @@ def Stat(stat):
     else:
         print colored('FAILED','red')
 
+def Print(msg):
+    print msg + '  --  ',
+
 ## Main program
 REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm'
 out=os.system("rpm -qa | grep -w zabbix-release &>/dev/null")
 if out != 0:
-    print 'Installing REPO packages',
+    Print('Installing REPO packages')
     out=os.system("rpm -ivh "+ REPO_URL + " &>/dev/null")
     Stat(out)
 
