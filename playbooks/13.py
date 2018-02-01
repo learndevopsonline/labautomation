49f9a0386170b314a2b367c2da6139808e1da628
Kalluru <sk185505@ncr.com>
2018-02-01 15:45:17 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -7,7 +7,5 @@ REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.e
 
 out=os.system("rpm -qa | grep -w kernel &>/dev/null")
 if out != 0:
-    print 'OK'
-else:
-    print 'NOTOK'
+    os.system("rpm -ivh "+ REPO_URL)
 
