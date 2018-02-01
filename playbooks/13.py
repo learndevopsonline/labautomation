7b5c9533075b4c791d741e4327039dc83dc7fa00
Kalluru <sk185505@ncr.com>
2018-02-01 16:23:46 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -4,15 +4,18 @@
 import os
 from termcolor import colored
 
+### Functions
+def Stat(stat):
+    if stat == 0:
+        print colored('SUCCESS', 'green')
+    else:
+        print colored('FAILED','red')
+
+## Main program
 REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm'
-
-print colored('hello', 'red'), colored('world', 'green')
-print "Nope, that is not a two. That is a",
-print 'hello hello'
-
 out=os.system("rpm -qa | grep -w zabbix-release &>/dev/null")
 if out != 0:
-    print ""
-    OSA=os.system("rpm -ivh "+ REPO_URL + " &>/dev/null")
-    #print OSA
+    print 'Installing REPO packages',
+    out=os.system("rpm -ivh "+ REPO_URL + " &>/dev/null")
+    Stat(out)
 
