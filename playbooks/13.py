a640a9fecd448343750b3ec86417a8a8882a4517
Kalluru <sk185505@ncr.com>
2018-02-01 15:34:13 +0530

    Code

--- playbooks/13.py
+++ playbooks/13.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 
-print 'Hello World'
+#print 'Hello World'
+import os
+
+REPO_URL='http://repo.zabbix.com/zabbix/3.4/rhel/7/x86_64/zabbix-release-3.4-2.el7.noarch.rpm'
+
+out=os.system("rpm -qa | grep -w packagename")
+print out
+
