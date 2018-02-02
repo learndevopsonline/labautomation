5c0eef3652e48109f653a83a9a88c6848c5af347
carreerit <rwn0517@gmail.com>
2018-02-02 12:00:28 +0530

    Code

--- playbooks/14.py
+++ playbooks/14.py
@@ -5,5 +5,5 @@ from urllib import urlopen
 
 url = "https://downloads.chef.io/chef-server/stable"    
 from stripogram import html2text
-text = html2text(url)
-print text
+html2text("https://downloads.chef.io/chef-server/stable")
+#print text
