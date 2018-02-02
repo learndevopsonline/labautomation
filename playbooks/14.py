222d31a79474442b1e717813c5860731a4a7bb1d
carreerit <rwn0517@gmail.com>
2018-02-02 15:41:26 +0530

    Code

--- playbooks/14.py
+++ playbooks/14.py
@@ -34,6 +34,6 @@ def processText(webpage):
 url = "https://downloads.chef.io/chef-server/stable"  
 html = urlopen(url).read()  
 raw=nltk.clean_html(html)
-text = html2text(raw)
+#text = html2text(raw)
 #print text
 processText(url)
