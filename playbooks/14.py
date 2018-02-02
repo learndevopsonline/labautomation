2958a5470aa80663b54bbf1d826cd60c500b444f
carreerit <rwn0517@gmail.com>
2018-02-02 12:04:24 +0530

    Code

--- playbooks/14.py
+++ playbooks/14.py
@@ -5,6 +5,6 @@ import urllib
 from stripogram import html2text
 
 url = "https://downloads.chef.io/chef-server/stable"  
-html = urllib.urlopen(url).read()  
+html = urlopen(url).read()  
 text = html2text(html)
 print text
