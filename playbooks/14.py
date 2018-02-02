48eebbcb40c7c76f9c4fbe1fa4042462708d155e
Raghu K <raghuk.vit@gmail.com>
2018-02-02 11:29:16 +0530

    Code

--- playbooks/14.py
+++ playbooks/14.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python 
 
-print 'hello'
+import nltk   
+from urllib import urlopen
+
+url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"    
+html = urlopen(url).read()    
+raw = nltk.clean_html(html)  
+print(raw)
