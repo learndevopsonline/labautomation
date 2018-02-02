f392c4afff4ea31f4e053f068259f76bfe88dc03
carreerit <rwn0517@gmail.com>
2018-02-02 12:05:15 +0530

    Code

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
 #source /root/scripts/common-functions.sh
 source /tmp/common-functions.sh
-
+echo $1
+exit
 
 option=$1
 if [ -z "$option" ]; then 
