0ce9689970cd56aca6d5e4e816f7574bf970776a
carreerit <rwn0517@gmail.com>
2018-02-02 15:37:30 +0530

    Code

--- playbooks/14.py
+++ playbooks/14.py
@@ -6,5 +6,6 @@ from urllib import urlopen
 
 url = "https://downloads.chef.io/chef-server/stable"  
 html = urlopen(url).read()  
-raw=nltk.clean_html(html)  
-print raw
+raw=nltk.clean_html(html)
+text = html2text(raw)
+print text
