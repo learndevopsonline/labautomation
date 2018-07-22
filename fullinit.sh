cafe5acd10deec51c809c5e32934c8b17e15fa49
Raghu K <raghuk.vit@gmail.com>
2018-07-22 21:43:36 +0530

    Updating Labauto

--- fullinit.sh
+++ fullinit.sh
@@ -1,1 +1,16 @@
-echo Hello World
+echo "Setting up Keys"
+cd ~
+ssh-keygen -f devops -t rsa -N ''
+mv devops devops.pem
+echo "Installing Google Cloud CLI"
+curl -s https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe -o GoogleCloudSDKInstaller.exe 
+./GoogleCloudSDKInstaller.exe 
+echo "Installing Python"
+OSVER=$(uname -m)
+if [ "$OSVER" == "x86_64" ]; then 
+    curl -s https://www.python.org/ftp/python/2.7.15/python-2.7.15.amd64.msi -o python-2.7.15.amd64.msi
+    ./python-2.7.15.amd64.msi
+else
+    curl -s https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi -o python-2.7.15.msi
+    ./python-2.7.15.msi
+fi
