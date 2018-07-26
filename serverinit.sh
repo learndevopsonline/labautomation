247e80b4b1460c17568503e2e26cf0946f7f71ed
Raghu K <raghuk.vit@gmail.com>
2018-07-26 15:00:46 +0530

    Update

--- serverinit.sh
+++ serverinit.sh
@@ -6,3 +6,21 @@ source /tmp/common-functions.sh
 CheckRoot
 
 CheckOS 7
+
+which gcloud &>/dev/null
+Stat $? "Checking Google Cloud CLI"
+
+HNAME=$(hostname)
+gcloud compute instances list | grep $HNAME &>/dev/null
+if [ $? -ne 0 ]; then 
+    PrintCenter "Please Open the coming URL, to create the and setup the LAB. Open the URL, and get the code and Paste it here"
+    echo -e "\e[33m"
+    gcloud auth login --quiet
+    gcloud compute instances list | grep $HNAME &>/dev/null
+    if [ $? -ne 0 ]; then 
+        Stat 1 "Provided Key is Wrong"
+    fi 
+fi 
+
+
+
