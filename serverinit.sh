e1be5842d0874462c27395a89cff75573e73cb1c
Raghu K <raghuk.vit@gmail.com>
2018-07-26 20:51:44 +0530

    Update

--- serverinit.sh
+++ serverinit.sh
@@ -66,7 +66,6 @@ ZONES=(us-east4-c us-central1-c us-west1-b europe-west1-b asia-east1-b asia-nort
 for servername in `curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/serverslist | dos2unix ` ; do 
    echo -e "Creating Server .. $servername"
    RNO=$(( ( RANDOM % 6 )  + 1 ))
-   echo $servername
    zone=${ZONES[$RNO]}
    gcloud compute instances create $servername --zone=$zone --machine-type=n1-standard-1 --image=mycentos7 --image-project=$PROJECT --boot-disk-size=10GB &>/dev/null
    gcloud compute instances stop $servername --quiet --zone=$zone &>/dev/null
