6993ba40a4bd37205bd46910cd34ecf07bcd23ab
Raghu K <raghuk.vit@gmail.com>
2018-07-26 16:58:19 +0530

    Update

--- serverinit.sh
+++ serverinit.sh
@@ -25,6 +25,29 @@ fi
 PROJECT=$(gcloud config list --format 'value(core.project)')
 IMAGE=$(gcloud compute images list | grep centos-7 | awk '{print $1}')
 
-gcloud compute instances create instance-2 --zone=us-east1-b --machine-type=n1-standard-1 --metadata=startup-script=curl\ -s\ https://raw.githubusercontent.com/linuxautomations/scripts/master/init.sh\ \|\ sudo\ bash --image=$IMAGE --image-project=centos-cloud --boot-disk-size=10GB #&>/dev/null
+info "Checking Pre-requisites"
+gcloud compute instances list 2>/dev/null | grep imaging &>/dev/null
+if [ $? -eq 0 ]; then 
+    gcloud compute instances delete imaging &>/dev/null 
+    sleep 60
+fi
+
+gcloud compute instances create imaging --zone=us-east1-b --machine-type=n1-standard-1 --metadata=startup-script=curl\ -s\ https://raw.githubusercontent.com/linuxautomations/scripts/master/init.sh\ \|\ sudo\ bash --image=$IMAGE --image-project=centos-cloud --boot-disk-size=10GB &>/dev/null
 Stat $? "Started making Image"
+
+
+while true ; do 
+a=1
+for i in \| \/ \- \\ \| \/ \- \\ ; do 
+    [ $a -gt 8 ] && break
+    (echo -n > /dev/tcp/instance-2/22 ) &>/dev/null
+    [ $? -eq 0 ] && A=0 && break 
+	echo -en "Waiting for SSH Connection .. $i \r"
+	sleep 0.25
+    a=$(($a+1))
+done
+[ -n "$A" ] && break
+done
+
+
 
