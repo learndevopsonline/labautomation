862c03d7391249b11c89367200a6bd14e6c8450d
Raghu K <raghuk.vit@gmail.com>
2018-07-26 16:22:42 +0530

    Update

--- serverinit.sh
+++ serverinit.sh
@@ -15,12 +15,16 @@ gcloud compute instances list 2>/dev/null| grep $HNAME &>/dev/null
 if [ $? -ne 0 ]; then 
     PrintCenter "Please Open the coming URL, to create the and setup the LAB. Open the URL, and get the code and Paste it here"
     echo -e "\e[33m"
-    gcloud auth login --quiet
+    gcloud auth login --quiet --brief
     gcloud compute instances list | grep $HNAME &>/dev/null
     if [ $? -ne 0 ]; then 
         Stat 1 "Provided Key is Wrong"
     fi 
 fi 
 
+PROJECT=$(gcloud config list --format 'value(core.project)')
+IMAGE=$(gcloud compute images list | grep centos-7 | awk '{print $1}')
 
+gcloud compute instances create instance-2 --zone=us-east1-b --machine-type=n1-standard-1 --metadata=startup-script=curl\ -s\ https://raw.githubusercontent.com/linuxautomations/scripts/master/init.sh\ \|\ sudo\ bash --image$IMAGE --image-project=centos-cloud --boot-disk-size=10GB &>/dev/null
+Stat $? "Started making Image"
 
