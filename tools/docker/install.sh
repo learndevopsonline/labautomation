b3385094b5e13764cc78111ddcd86735e432cb60
Raghu K <raghukx@gmail.com>
2019-02-26 18:50:06 +0530

    Update install.sh

--- tools/docker/install.sh
+++ tools/docker/install.sh
@@ -6,5 +6,6 @@ usermod -a -G docker $USERNAME
 yum install docker -y 
 systemctl enable docker
 sed -i -e '/ExecStart/ c ExecStart=/usr/bin/dockerd-current -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock \\' /lib/systemd/system/docker.service
+systemctl daemon-reload
 systemctl start docker 
 sudo su - centos 
