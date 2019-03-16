64face4b4f3450b1c5ac0d9f590518685ca2fdeb
Raghu K <raghukx@gmail.com>
2019-03-16 08:15:14 +0530

    Update install.sh

--- tools/maven/install.sh
+++ tools/maven/install.sh
@@ -5,4 +5,4 @@ cd /opt
 curl -s https://archive.apache.org/dist/maven/maven-3/${VERSION}/binaries/apache-maven-${VERSION}-bin.zip -o /tmp/apache-maven-${VERSION}-bin.zip
 unzip /tmp/apache-maven-${VERSION}-bin.zip
 mv apache-maven-${VERSION} maven
-ls -s /opt/maven/bin/mvn /bin/mvn
+ln -s /opt/maven/bin/mvn /bin/mvn
