66ee21ff3f50f177a271493ccb03cb3e7bbe757c
Raghu K <raghukx@gmail.com>
2019-07-08 06:52:46 +0530

    Update install.sh

--- tools/maven/install.sh
+++ tools/maven/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $(id -u) -ne 0 ]; then 
+  echo "You should be a root user / sudo user to perform this script"
+  exit 1
+fi 
+
+yum install java-1.8.0-openjdk-devel -y
+
 VERSION=$(curl -s https://maven.apache.org/download.cgi  | grep Downloading |awk '{print $NF}' |awk -F '<' '{print $1}')
 cd /opt
 curl -s https://archive.apache.org/dist/maven/maven-3/${VERSION}/binaries/apache-maven-${VERSION}-bin.zip -o /tmp/apache-maven-${VERSION}-bin.zip
