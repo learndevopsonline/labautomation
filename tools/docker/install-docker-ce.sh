4b03a7a21cc3412fb8cd45657577cd22fe0312f9
Raghu K <raghukx@gmail.com>
2019-07-24 21:31:22 +0530

    Update install-docker-ce.sh

--- tools/docker/install-docker-ce.sh
+++ tools/docker/install-docker-ce.sh
@@ -8,5 +8,16 @@ fi
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli containerd.io -y
+
+## Group add 
+id centos &>/dev/null
+if [ $? -eq 0 ]; then 
+  groupadd docker &>/dev/null
+  usermod -a -G docker centos
+fi
+
 systemctl enable docker 
 systemctl start docker
+
+echo -e "\e[33m \n\t --> You need to relogin and run the following command <-- \e[0m"
+echo -e "    docker ps    "
