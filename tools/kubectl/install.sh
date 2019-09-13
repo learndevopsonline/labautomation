2553e3b29754dbbbbcadb11a95ceef76aee6eaef
KalluruS <sriharshak@INSriharshaks-MacBook-Pro.local>
2019-09-13 16:22:06 +0530

    Update kunbcetl Script

--- tools/kubectl/install.sh
+++ tools/kubectl/install.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+## Check Root User
+
+if [ $(id -u) -ne 0 ]; then
+  echo "You should be root user to perform this script"
+  exit 1
+fi
+
+curl -L https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl -o /bin/kubectl
+chmod +x /bin/kubectl
