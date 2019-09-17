3773841971a3c40ad5632616ba1b944ed8b6953a
KalluruS <>
2019-09-17 15:44:31 +0530

    --- Auto-Git Commit ---

--- tools/sonarqube/install.sh
+++ tools/sonarqube/install.sh
@@ -7,4 +7,5 @@ fi
 
 URL=$(curl -s https://www.sonarqube.org/downloads/ | grep 'Community Edition' | head -1  | xargs -n 1  | grep ^href | awk -F = '{print $2}')
 id sonar &>/dev/null 
+if [ $? -eq 0]; then 
 useradd sonar 
