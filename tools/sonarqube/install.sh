f53610b284d9b081e1dc47b0a88b7661226750e6
KalluruS <>
2019-09-17 15:41:10 +0530

    --- Auto-Git Commit ---

--- tools/sonarqube/install.sh
+++ tools/sonarqube/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
-
+if [ $(id -u) -eq 0 ]; then 
+  
 URL=$(curl -s https://www.sonarqube.org/downloads/ | grep 'Community Edition' | head -1  | xargs -n 1  | grep ^href | awk -F = '{print $2}')
