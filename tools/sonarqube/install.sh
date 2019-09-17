f04ad14ed5deaa957b7208bd74bf4b6192291b27
KalluruS <>
2019-09-17 16:53:18 +0530

    --- Auto-Git Commit ---

--- tools/sonarqube/install.sh
+++ tools/sonarqube/install.sh
@@ -27,4 +27,5 @@ chown sonar:sonar sonarqube -R
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/sonarqube/sonar.service >/etc/systemd/system/sonarqube.service 
 systemctl daemon-reload 
 systemctl enable sonarqube 
-sed -i -e '/^RUN_AS_USER/ d' -e '/#RUN_AS_USER/ RUN_AS_USER=sonar' 
+sed -i -e '/^RUN_AS_USER/ d' -e '/#RUN_AS_USER/ RUN_AS_USER=sonar' /home/sonar/sonarqube/bin/linux-x86-64/sonar.sh
+systemctl start sonarqube
