b71b3436f210bc5d9506d4da5e260932d9f47a3a
KalluruS <>
2019-09-17 15:41:30 +0530

    --- Auto-Git Commit ---

--- tools/sonarqube/install.sh
+++ tools/sonarqube/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
 if [ $(id -u) -eq 0 ]; then 
-  
+  echo "You should be a root user to perform this command"
+  exit 1 
+fi 
+
 URL=$(curl -s https://www.sonarqube.org/downloads/ | grep 'Community Edition' | head -1  | xargs -n 1  | grep ^href | awk -F = '{print $2}')
+curl 
