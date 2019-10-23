c07df91918487ea2929b623bfe0b160c59dbbb7a
KalluruS <>
2019-10-23 15:32:06 +0530

    --- Auto-Git Commit ---

--- tools/docker-compose/install.sh
+++ tools/docker-compose/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 VERSION=$(curl -s https://github.com/docker/compose/tags | grep tar.gz | grep -v '\-rc' | grep nofollow | head -1 |awk -F / '{print $NF}' | awk '{print $1}' | sed -e 's/.tar.gz"//')
-curl -s -L "https://github.com/docker/compose/releases/download//docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -s -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
