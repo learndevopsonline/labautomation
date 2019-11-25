3718e3f24d8634f4b4a1634fc3790ce1918ce84d
Raghu K <raghukx@gmail.com>
2019-11-25 08:35:38 +0530

    Create install.sh

---
+++ tools/aws-iam-auth/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then 
+  echo "You should run as root user"
+  exit 1
+fi
+URL=$(curl -s https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html | grep curl  | grep amd64 | head -1 | sed -e 's|</code>| |g' | xargs -n1 | grep ^http)
+curl -s $URL -o /bin/aws-iam-authenticator
+chmod +x /bin/aws-iam-authenticator
