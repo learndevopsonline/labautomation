4c1813d96cb0ceb0f3f94760f9e8fab71c570685
KalluruS <>
2019-11-27 06:52:55 +0530

    --- Auto-Git Commit ---

--- tools/awx/install.sh
+++ tools/awx/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
 Stat() {
-  if [ $1 -eq 0 ]; then
+  if [ $1 -ne 0 ]; then
+    echo -e ""
 }
 
 echo "Note: This script is made to run on Centos 7 OS.. Cannot gaurantee on other OS's"
