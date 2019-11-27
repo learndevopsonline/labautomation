e305dfb2940b49c8011da8abf1934c0d81b6cd68
KalluruS <>
2019-11-27 06:52:34 +0530

    --- Auto-Git Commit ---

--- tools/awx/install.sh
+++ tools/awx/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
+
 echo "Note: This script is made to run on Centos 7 OS.. Cannot gaurantee on other OS's"
 sleep 5
 
@@ -8,3 +9,4 @@ rm -f $LOG
 
 echo -e "Installing Python 3" 
 yum install python36 python36-devel -y &>>$LOG 
+Stat $?
