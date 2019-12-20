d2f35c43c2debfaa8c31c9e509521dd908618fa1
KalluruS <>
2019-12-20 08:19:34 +0530

    --- Auto-Git Commit ---

--- tools/mongodb/install.sh
+++ tools/mongodb/install.sh
@@ -12,4 +12,5 @@ name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb-org-4.2.repo 
+
