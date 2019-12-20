913e0de199268c67d4e1048015414784e8ef7bf6
KalluruS <>
2019-12-20 08:18:44 +0530

    --- Auto-Git Commit ---

--- tools/mongodb/install.sh
+++ tools/mongodb/install.sh
@@ -2,4 +2,8 @@
 
 curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
 #source /root/scripts/common-functions.sh
-source /tmp/common-functions.sh
+source /tmp/common-functions.sh 
+
+
+CheckRoot 
+
