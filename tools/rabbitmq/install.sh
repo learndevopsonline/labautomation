81c5d9de06ea8e86259875b653bb44c274a5d2b0
KalluruS <>
2019-12-20 08:31:06 +0530

    --- Auto-Git Commit ---

---
+++ tools/rabbitmq/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
