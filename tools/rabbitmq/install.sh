e8f7369ba695c6d69ee8bf5aaa272d67d8ad519c
KalluruS <>
2019-12-20 08:31:56 +0530

    --- Auto-Git Commit ---

--- tools/rabbitmq/install.sh
+++ tools/rabbitmq/install.sh
@@ -11,3 +11,7 @@ CheckRoot
 yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y 
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo 
+
+yum install rabbitmq-server -y 
+systemctl enable rabbitmq-server 
+systemctl start rabbitmq-server 
