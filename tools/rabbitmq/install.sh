b070bb4bd9b3c24cfadedb7eb627367e2d1be37e
KalluruS <>
2019-12-20 08:45:18 +0530

    --- Auto-Git Commit ---

--- tools/rabbitmq/install.sh
+++ tools/rabbitmq/install.sh
@@ -4,10 +4,6 @@ bashcurl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/c
 #source /root/scripts/common-functions.sh
 source /tmp/common-functions.sh 
 
-
-CheckRoot 
-
-
 yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y 
 
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo 
