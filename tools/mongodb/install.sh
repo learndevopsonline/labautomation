aae96cb8a3aeead598e1fe3af6952844a867b9cf
KalluruS <>
2019-12-22 13:05:48 +0530

    --- Auto-Git Commit ---

--- tools/mongodb/install.sh
+++ tools/mongodb/install.sh
@@ -15,7 +15,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb-org-4.2.repo 
 
 yum install -y mongodb-org
-sed -i -e 's/127.0.0.1/0.0.0.0' 
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 
 systemctl start mongod 
 systemctl enable mongod
