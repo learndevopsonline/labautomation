cf95426a0a5dc2c61dab74db30da718537759d6a
KalluruS <>
2019-12-22 13:18:30 +0530

    --- Auto-Git Commit ---

--- tools/rabbitmq/install.sh
+++ tools/rabbitmq/install.sh
@@ -11,3 +11,5 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 yum install rabbitmq-server -y 
 systemctl enable rabbitmq-server 
 systemctl start rabbitmq-server 
+
+## Access setup for Application 
