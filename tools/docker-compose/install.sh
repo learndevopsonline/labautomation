d55d8c721875b394a134762f23928a82086433d6
KalluruS <>
2019-12-23 21:13:54 +0530

    --- Auto-Git Commit ---

--- tools/docker-compose/install.sh
+++ tools/docker-compose/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 VERSION=$(curl -s https://github.com/docker/compose/tags | grep tar.gz | grep -v '\-rc' | grep nofollow | head -1 |awk -F / '{print $NF}' | awk '{print $1}' | sed -e 's/.tar.gz"//')
-curl -s -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+curl -s -L "https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
+chmod +x /bin/docker-compose
 docker-compose --version &>/dev/null 
 if [ $? -eq 127 -o $? -eq 126 ]; then 
   echo -e "\e[31m Installation Failed"
