919d57cf8d6e3db706083eb9928a0f75f83a2a54
KalluruS <>
2020-02-03 07:23:31 +0530

    --- Auto-Git Commit ---

--- tools/gradle/install.sh
+++ tools/gradle/install.sh
@@ -8,3 +8,4 @@ fi
 yum install java-1.8.0-openjdk-devel unzip  -y
 
 DOWNLOAD_URL=$(curl -s https://gradle.org/releases/  | grep bin.zip | head -1 |xargs -n1 | grep ^href | awk -F = '{print $2}')
+cd /opt 
