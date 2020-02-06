71e8d47d817380feef893756a8158261e57c43b8
KalluruS <>
2020-02-06 06:21:35 +0530

    --- Auto-Git Commit ---

--- tools/docker-stack/install.sh
+++ tools/docker-stack/install.sh
@@ -4,3 +4,4 @@ echo -e "\e[32m Installing Docker \e[0m"
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install-docker-ce.sh | sudo bash 
 
 echo -e "\e[32m Installing Docker Compose \e[0m"
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker-compose/install.sh | sudo bash 
