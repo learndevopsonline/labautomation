e5e28425f270a39343bf524e04e751287f3b2faa
KalluruS <>
2020-02-07 07:15:55 +0530

    --- Auto-Git Commit ---

--- tools/mysql/install-5.7.sh
+++ tools/mysql/install-5.7.sh
@@ -3,4 +3,6 @@
 wget -q https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
 tar -xf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
 yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm mysql-community-common-5.7.28-1.el7.x86_64.rpm mysql-community-libs-5.7.28-1.el7.x86_64.rpm mysql-community-server-5.7.28-1.el7.x86_64.rpm -y 
-if [ $? -eq 0 ]
+if [ $? -eq 0 ]; then 
+  rm -f mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar *.rpm 
+  
