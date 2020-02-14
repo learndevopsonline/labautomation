350defe158bc9894947d9aa601f685bf7a4f2913
r-devops <raghuk.vit@gmail.com>
2020-02-14 07:10:09 +0530

    Update install-5.7.sh

--- tools/mysql/install-5.7.sh
+++ tools/mysql/install-5.7.sh
@@ -1,5 +1,7 @@
 #!bin/bash 
 
+yum remove mariadb-libs -y 
+
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
 tar -xf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
 yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm mysql-community-common-5.7.28-1.el7.x86_64.rpm mysql-community-libs-5.7.28-1.el7.x86_64.rpm mysql-community-server-5.7.28-1.el7.x86_64.rpm -y 
