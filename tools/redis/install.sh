5a07aae6101346719397dca27d7ede1af8630da3
KalluruS <>
2020-02-14 07:25:36 +0530

    --- Auto-Git Commit ---

--- tools/redis/install.sh
+++ tools/redis/install.sh
@@ -4,11 +4,11 @@
 
 yum install epel-release yum-utils
 yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-sudo yum-config-manager --enable remi
-Install the Redis package by typing:
+yum-config-manager --enable remi
+
 
 sudo yum install redis
-Once the installation is completed, start the Redis service and enable it to start automatically on boot with:
+
 
 sudo systemctl start redis
 sudo systemctl enable redis
