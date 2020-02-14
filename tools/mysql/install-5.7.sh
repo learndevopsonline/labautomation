a5188f43ea063c5be9c266bd0d64390aac3b08db
r-devops <raghuk.vit@gmail.com>
2020-02-14 07:20:37 +0530

    Update install-5.7.sh

--- tools/mysql/install-5.7.sh
+++ tools/mysql/install-5.7.sh
@@ -2,6 +2,11 @@
 
 yum remove mariadb-libs -y 
 
+rpm -qa | grep mysql-community-server &>/dev/null 
+if [ $? -eq 0 ]; then 
+  echo "MySQL Install - Skipping"
+  exit 0
+fi
 wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
 tar -xf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar 
 yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm mysql-community-common-5.7.28-1.el7.x86_64.rpm mysql-community-libs-5.7.28-1.el7.x86_64.rpm mysql-community-server-5.7.28-1.el7.x86_64.rpm -y 
