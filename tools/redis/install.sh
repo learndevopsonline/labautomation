f39a08f885125c1ac215927dd0255df8b5c2cd5e
KalluruS <>
2020-02-14 07:32:17 +0530

    --- Auto-Git Commit ---

--- tools/redis/install.sh
+++ tools/redis/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-yum install epel-release yum-utils
+yum install epel-release yum-utils -y
 yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 yum-config-manager --enable remi
 yum install redis -y
