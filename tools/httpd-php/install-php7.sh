4689d4ab61285d5558debfaadf73f734eb2ffae1
KalluruS <>
2020-02-18 07:07:11 +0530

    --- Auto-Git Commit ---

--- tools/httpd-php/install-php7.sh
+++ tools/httpd-php/install-php7.sh
@@ -5,4 +5,5 @@ yum install  http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 yum -y install yum-utils
 
 yum-config-manager --enable remi-php73
-yum -y install php php-opcache php73-php-pdo composer
+yum -y install php php-opcache php73-php-pdo composerCREATE USER 'jeffrey'@'localhost' IDENTIFIED BY 'password';
+
