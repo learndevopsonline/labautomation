fbaf09f8883d3e104e93b75dda9cd66b410fa2bb
KalluruS <>
2020-02-20 08:24:12 +0530

    --- Auto-Git Commit ---

--- tools/nexus/install.sh
+++ tools/nexus/install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
 
+if [ -z "$CERT_NAME" ]; then 
+  echo "Certificate Name is missing, export CERT_NAME and run again!!"
+  exit 1
+fi 
+
 curl -s https://raw.githubusercontent.com/linuxautomations/nexus/master/install.sh | sudo bash 
