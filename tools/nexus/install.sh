92f5eb87561dd7050c3edb8f28423d9baae93f4d
KalluruS <>
2020-02-20 08:23:22 +0530

    --- Auto-Git Commit ---

--- tools/nexus/install.sh
+++ tools/nexus/install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash 
 
 curl -s https://raw.githubusercontent.com/linuxautomations/nexus/master/install.sh | sudo bash 
-
-curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/letsencrypt/create-cert.sh | bash 
