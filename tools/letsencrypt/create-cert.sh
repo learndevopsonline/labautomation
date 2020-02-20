da6f6746940eed3a8c68a079ff7a447bb75af65b
KalluruS <>
2020-02-20 08:04:49 +0530

    --- Auto-Git Commit ---

--- tools/letsencrypt/create-cert.sh
+++ tools/letsencrypt/create-cert.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
 if [ -z "CERT_NAME" ]; then 
-  echo "Certificate Name is missing, export CERT_NAME and run again"
+  echo "Certificate Name is missing, export CERT_NAME and run again!!"
+  exit 1
 fi 
