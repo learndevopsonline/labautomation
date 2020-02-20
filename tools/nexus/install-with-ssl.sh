5072a6252fab7671c4d88dab49da609c72aa5da2
KalluruS <>
2020-02-20 08:25:12 +0530

    --- Auto-Git Commit ---

--- tools/nexus/install-with-ssl.sh
+++ tools/nexus/install-with-ssl.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
-
+if [ -z "$CERT_NAME" ]; then 
+  echo "Certificate Name is missing, export CERT_NAME and run again!!"
+  exit 1
+fi 
 
 curl -s https://raw.githubusercontent.com/linuxautomations/nexus/master/install.sh | sudo bash 
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/letsencrypt/create-cert.sh | bash 
+
