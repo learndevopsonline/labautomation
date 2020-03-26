923ea5ea7cff6ebca46537dc813e46c30b338ca0
sriharshakx <57408914+sriharshakx@users.noreply.github.com>
2020-03-26 11:55:26 +0530

    Update install.sh

--- tools/terraform/install.sh
+++ tools/terraform/install.sh
@@ -3,4 +3,8 @@
 URL=$(curl https://www.terraform.io/downloads.html  | grep 64-bit | grep linux_amd64.zip | awk -F \" '{print $2}')
 FILE=$(echo $URL | awk -F / '{print $NF}')
 curl -s -L -J -O $URL 
+sudo yum install unzip -y 
 unzip $FILE -d /bin
+if [ $? -eq 0 ]; then 
+  echo -e "\e[31m SUCCESS\e[0m"
+fi 
