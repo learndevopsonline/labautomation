0e049713a16b7f2ec2d90d32fa285edc6cbac19b
KalluruS <>
2020-03-29 11:09:46 +0530

    --- Auto-Git Commit ---

--- tools/go/install.sh
+++ tools/go/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 GO_VERSION=$(curl -L -s https://golang.org/dl/   | grep toggleVisible | head -1 | sed -e 's/"/ /g' |xargs -n 1  | grep ^go)
-GO_URL=""
+GO_URL="ttps://dl.google.com/go/${}.linux-amd64.tar.gz"
