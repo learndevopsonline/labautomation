227a35bad513c606af364988d6aff117652c2433
KalluruS <>
2020-04-04 06:25:44 +0530

    --- Auto-Git Commit ---

--- tools/elk/install.sh
+++ tools/elk/install.sh
@@ -21,23 +21,24 @@ Print() {
   echo -e "${Y}${1}${N}"
 }
 
-if [ "$SKIP" == "TRUE" ]
-## Install html2text 
-Print "Installing Html2text"
+if [ "$SKIP" == "TRUE" ]; then 
+  ## Install html2text 
+  Print "Installing Html2text"
 
-yum install https://li.nux.ro/download/nux/dextop/el7/x86_64/html2text-1.3.2a-14.el7.nux.x86_64.rpm -y  &>/dev/null
-STAT $?
-VERSION=$(curl -s -L https://www.elastic.co/downloads/elasticsearch  | html2text  | grep Version -A 1 | tail -1)
+  yum install https://li.nux.ro/download/nux/dextop/el7/x86_64/html2text-1.3.2a-14.el7.nux.x86_64.rpm -y  &>/dev/null
+  STAT $?
+  VERSION=$(curl -s -L https://www.elastic.co/downloads/elasticsearch  | html2text  | grep Version -A 1 | tail -1)
 
-Print "Installing Elasicsearch"
-yum install https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${VERSION}-x86_64.rpm &>/dev/null 
-STAT $? 
+  Print "Installing Elasicsearch"
+  yum install https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${VERSION}-x86_64.rpm &>/dev/null 
+  STAT $? 
 
-Print "Installing Kibana"
-yum install https://artifacts.elastic.co/downloads/kibana/kibana-${VERSION}-x86_64.rpm -y &>/dev/null
-STAT $? 
+  Print "Installing Kibana"
+  yum install https://artifacts.elastic.co/downloads/kibana/kibana-${VERSION}-x86_64.rpm -y &>/dev/null
+  STAT $? 
 
-Print "Installing Logstash"
-yum install https://artifacts.elastic.co/downloads/logstash/logstash-${VERSION}.rpm -y &>/dev/nulll 
-STAT $? 
+  Print "Installing Logstash"
+  yum install https://artifacts.elastic.co/downloads/logstash/logstash-${VERSION}.rpm -y &>/dev/nulll 
+  STAT $? 
+fi 
 
