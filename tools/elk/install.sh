de3797dcb2b31dd2a6517289e8fc665f5aeccea3
KalluruS <>
2020-04-04 06:40:57 +0530

    --- Auto-Git Commit ---

--- tools/elk/install.sh
+++ tools/elk/install.sh
@@ -56,7 +56,7 @@ output {
     hosts => ["http://localhost:9200"]
     index => "%{[@metadata][beat]}-%{[@metadata][version]}" 
   }
-}' >/etc/logstash
+}' >/etc/logstash/conf.d/
 
 yum install nginx -y &>/dev/null 
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/elk/http-proxy.conf >/etc/nginx/nginx.conf 
