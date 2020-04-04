d0fb4990072bb888582575c8320dc67251068019
KalluruS <>
2020-04-04 06:21:04 +0530

    --- Auto-Git Commit ---

--- tools/elk/install.sh
+++ tools/elk/install.sh
@@ -12,7 +12,7 @@ STAT() {
 
 ## Install html2text 
 
-echo -e "${Y}Installing Html2text${N}"
+
 yum install https://li.nux.ro/download/nux/dextop/el7/x86_64/html2text-1.3.2a-14.el7.nux.x86_64.rpm -y  &>/dev/null
 STAT $?
 VERSION=$(curl -s -L https://www.elastic.co/downloads/elasticsearch  | html2text  | grep Version -A 1 | tail -1)
