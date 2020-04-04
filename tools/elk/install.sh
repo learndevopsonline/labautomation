ace0d2f10ff51cdf2aa9e14a1db0916f8f0cc68b
KalluruS <>
2020-04-04 06:21:24 +0530

    --- Auto-Git Commit ---

--- tools/elk/install.sh
+++ tools/elk/install.sh
@@ -15,7 +15,7 @@ Print() {
 }
 
 ## Install html2text 
-
+Print "Installing Html2text"
 
 yum install https://li.nux.ro/download/nux/dextop/el7/x86_64/html2text-1.3.2a-14.el7.nux.x86_64.rpm -y  &>/dev/null
 STAT $?
