a060b972d3c7bc183c87d95f29f8ac8e1bf186f3
Raghu K <raghukx@gmail.com>
2020-04-07 21:46:23 +0530

    Create install.sh

---
+++ tools/k9s/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+cd /tmp
+curl -L https://github.com/derailed/k9s/releases/download/$(curl -s https://github.com/derailed/k9s/releases | grep 'Release v' | head -1 | sed -e 's|<h1>||' -e 's|</h1>||' | awk '{print $2}')/k9s_Linux_x86_64.tar.gz | tar -xz
+sudo mv k9s /bin
