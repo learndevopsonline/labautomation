994b13a1bb738314b07bc1510a7f45501ed779c4
Raghu K <raghukx@gmail.com>
2020-04-07 21:46:42 +0530

    Create install.sh

---
+++ tools/k8-client-stack/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/kubectl/install.sh | sudo bash 
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/kubens/install.sh | sudo bash 
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/k9s/install.sh | sudo bash
