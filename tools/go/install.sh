c2aa417fb8d52007e8daaa7da4ea3e43dbeffdc3
Raghu K <raghukx@gmail.com>
2020-04-17 06:20:40 +0530

    Update install.sh

--- tools/go/install.sh
+++ tools/go/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 GO_VERSION=$(curl -L -s https://golang.org/dl/   | grep toggleVisible | head -1 | sed -e 's/"/ /g' |xargs -n 1  | grep ^go)
-GO_URL="ttps://dl.google.com/go/${GO_VERSION}.linux-amd64.tar.gz"
+GO_URL="https://dl.google.com/go/${GO_VERSION}.linux-amd64.tar.gz"
 
 cd /opt 
 [ -d /opt/go ] && rm -rf /opt/go
