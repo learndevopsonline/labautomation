b476d6fe77d4beba73c651a66ee84056d5f307b6
KalluruS <>
2020-04-28 21:32:50 +0530

    --- Auto-Git Commit ---

--- tools/eksctl/install.sh
+++ tools/eksctl/install.sh
@@ -2,4 +2,4 @@
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
-sudo mv /tmp/eksctl /usr/local/bin
+sudo mv /tmp/eksctl /bin
