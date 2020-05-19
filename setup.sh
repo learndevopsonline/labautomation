26343b5df9dc1e0e18f09cea517897c2f27edff8
KalluruS <>
2020-05-19 22:06:41 +0530

    ss

--- setup.sh
+++ setup.sh
@@ -10,6 +10,9 @@ else
 	cd /tmp/labauto 
 	git pull &>/dev/null
 fi 
+
+echo ">>>>> Select a TOOL to Install"
+export PS3="Select Tool> "
 select tool in `ls -1 /tmp/labauto/tools`; do 
 	echo Hai
 done
