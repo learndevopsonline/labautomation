c3a61db215ca845c9af24dd04f03aae97fda9a2b
KalluruS <>
2020-05-28 13:37:58 +0530

    ss

--- tools/nodejs/install.sh
+++ tools/nodejs/install.sh
@@ -7,6 +7,8 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi 
 
+yum install gcc-c++ -y 
+
 URL=$(curl -s https://nodejs.org/en/download/ | xargs -n1 | grep linux-x64.tar | sed -e 's|=| |g' -e 's|>| |g'| xargs -n1 | grep ^http)
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
 FOLDER_NAME=$(echo $FILENAME |sed -e 's/.tar.xz//')
