52e61c4edbed2e67de5cf12ca6f0b12b49d11c31
Raghu K <raghukx@gmail.com>
2020-08-05 20:52:53 +0530

    Update installv3.sh

--- tools/helm/installv3.sh
+++ tools/helm/installv3.sh
@@ -2,4 +2,4 @@
 
 cd /tmp 
 curl -L https://get.helm.sh/helm-$(curl -s https://github.com/helm/helm/releases  | grep muted-link | grep v3 | head -1 | awk '{print $NF}' | awk -F \" '{print $2}')-linux-amd64.tar.gz | tar -xz
-sudo mv linux-amd64/helm /usr/local/bin || mv linux-amd64/helm /usr/local/bin
+mv linux-amd64/helm /usr/local/bin
