23bd33001e65706f74b95e9313a717e4da45d140
r-devops <raghuk.vit@gmail.com>
2020-08-26 22:14:46 +0530

    Update install.sh

--- tools/sonarqube/install.sh
+++ tools/sonarqube/install.sh
@@ -1,31 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
 
-if [ $(id -u) -ne 0 ]; then 
+if [ $(id -u) -ne 0 ]; then
   echo "You should be a root user to perform this command"
-  exit 1 
-fi 
+  exit 1
+fi
 
-yum install java-11-openjdk -y 
-URL=$(curl -s https://www.sonarqube.org/downloads/ | grep 'Community Edition' | head -1  | xargs -n 1  | grep ^href | awk -F = '{print $2}')
+yum install java-11-openjdk -y
+URL=$(curl -s https://www.sonarqube.org/downloads/ | grep 'Community Edition' | grep sonarqube-|head -1  | sed -e "s/>/ /g"|xargs -n 1  | grep ^href | awk -F = '{print $2}')
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
 FOLDERNAME=$(echo $FILENAME | sed -e 's/.zip//g')
 
-id sonar &>/dev/null 
-if [ $? -ne 0 ]; then 
-  useradd sonar 
-fi 
+id sonar &>/dev/null
+if [ $? -ne 0 ]; then
+  useradd sonar
+fi
 
-cd /home/sonar 
-pkill java 
-rm -rf sonarqube 
-curl -s -o $FILENAME $URL 
-unzip $FILENAME 
+cd /home/sonar
+pkill java
+rm -rf sonarqube
+curl -s -o $FILENAME $URL
+unzip $FILENAME
 rm -f $FILENAME
 mv $FOLDERNAME sonarqube
 
-chown sonar:sonar sonarqube -R 
-curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/sonarqube/sonar.service >/etc/systemd/system/sonarqube.service 
-systemctl daemon-reload 
-systemctl enable sonarqube 
+chown sonar:sonar sonarqube -R
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/sonarqube/sonar.service >/etc/systemd/system/sonarqube.service
+systemctl daemon-reload
+systemctl enable sonarqube
 sed -i -e '/^RUN_AS_USER/ d' -e '/#RUN_AS_USER/ a RUN_AS_USER=sonar' /home/sonar/sonarqube/bin/linux-x86-64/sonar.sh
 systemctl start sonarqube
