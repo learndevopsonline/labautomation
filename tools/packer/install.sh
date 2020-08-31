57d8261a5d4d4b929b0f90ed72e904a0bf7f4b9f
KalluruS <>
2020-08-31 21:54:09 +0530

    ss

---
+++ tools/packer/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+VERSION=$(curl -s https://releases.hashicorp.com/packer/  | grep packer_ | head -1|sed -e 's|<| |g' -e 's|>| |g' | xargs -n1  | grep packer_ | sed -e 's/packer_//')
+
+curl -s https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip -o /tmp/packer.zip 
+cd /bin 
+unzip -o /tmp/packer.zip 
