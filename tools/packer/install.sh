03a57923038299f8f74d19a4444f942d9797f1ba
KalluruS <>
2020-09-30 07:05:23 +0530

    ss

--- tools/packer/install.sh
+++ tools/packer/install.sh
@@ -3,5 +3,6 @@
 VERSION=$(curl -s https://releases.hashicorp.com/packer/  | grep packer_ | head -1|sed -e 's|<| |g' -e 's|>| |g' | xargs -n1  | grep packer_ | sed -e 's/packer_//')
 
 curl -s https://releases.hashicorp.com/packer/${VERSION}/packer_${VERSION}_linux_amd64.zip -o /tmp/packer.zip 
+unlink /sbin/packer
 cd /bin 
 unzip -o /tmp/packer.zip 
---
+++ tools/prometheus-server/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then 
+  echo "You should run as root user"
+  exit 1
+fi 
+
+if [ -d /opt/prometheus ]; then 
+  rm -rf /opt/prometheus
+fi 
+
+URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep prometheus- | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http)
+
+FILENAME=$(echo $URL | awk -F / '{print $NF}')
+DIRNAME=$(echo $FILENAME | sed -e 's/.tar.gz//')
+
+cd /opt
+curl -s -L -O $URL 
+tar -xf $FILENAME 
+rm -f $FILENAME 
+mv $DIRNAME prometheus 
