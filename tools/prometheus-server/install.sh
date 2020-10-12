bbbc1b8e8f2bfdbe3ae04ebf06f8e4d3e8581be7
r-devops <raghuk.vit@gmail.com>
2020-10-12 20:36:38 +0530

    Update install.sh

--- tools/prometheus-server/install.sh
+++ tools/prometheus-server/install.sh
@@ -9,7 +9,7 @@ if [ -d /opt/prometheus ]; then
   rm -rf /opt/prometheus
 fi 
 
-URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep prometheus- | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http)
+URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep prometheus- | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http | tail -1)
 
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
 DIRNAME=$(echo $FILENAME | sed -e 's/.tar.gz//')
