ab8ecc37e5ee17c95e1c7cde52b172dd26b7a407
KalluruS <>
2021-03-28 11:50:26 +0530

    ss

--- setup.sh
+++ setup.sh
@@ -4,21 +4,21 @@ curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/commo
 #source /root/scripts/common-functions.sh
 source /tmp/common-functions.sh
 
-if [ ! -d /tmp/labauto  ]; then 
-	git clone https://github.com/linuxautomations/labautomation.git /tmp/labauto &>/dev/null
+if [ ! -d /tmp/labautomation  ]; then 
+	git clone https://github.com/linuxautomations/labautomation.git /tmp/labautomation &>/dev/null
 else 
-	cd /tmp/labauto 
+	cd /tmp/labautomation 
 	git pull &>/dev/null
 fi 
 
 echo -e "${Y}>>>>> Select a TOOL to Install${N}"
 export PS3="Select Tool> "
-select tool in `ls -1 /tmp/labauto/tools`; do 
-	SCRIPT_NO=$(ls /tmp/labauto/tools/$tool/*.sh |wc -l)
+select tool in `ls -1 /tmp/labautomation/tools`; do 
+	SCRIPT_NO=$(ls /tmp/labautomation/tools/$tool/*.sh |wc -l)
 	case $SCRIPT_NO in 
 		1) 
 			echo -e "\e[1;33m★★★ Installing $tool ★★★\e[0m"
-			sh /tmp/labauto/tools/$tool/install.sh
+			sh /tmp/labautomation/tools/$tool/install.sh
 			;;
 		0) 
 			echo -e "No Install Script Found"
@@ -26,9 +26,9 @@ select tool in `ls -1 /tmp/labauto/tools`; do
 			;;
 		*) 
 			echo -e "\e[31m Found Multiple Scripts, Choose One.. "
-			select script in `ls -1 /tmp/labauto/tools/$tool/*.sh | awk -F / '{print $NF}'`; do 
+			select script in `ls -1 /tmp/labautomation/tools/$tool/*.sh | awk -F / '{print $NF}'`; do 
 				echo -e "\e[1;33m★★★ Installing $tool ★★★\e[0m"
-				sh /tmp/labauto/tools/$tool/$script 
+				sh /tmp/labautomation/tools/$tool/$script 
 				break 
 			done
 			;; 
