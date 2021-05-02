7e11b99d57018e4873517a11f11810db4d8b4f80
KalluruS <>
2021-05-02 22:23:06 +0530

    ss

--- tools/todoapp-validate/main.go
+++ tools/todoapp-validate/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+  resp, err := http.Get("ifconfig.co")
+  if err != nil {
+    fmt.Println("Problem with Error")
+  }
+  fmt.Println("Print Ip Address"+ resp)
+  defer resp.Body.Close()
 }
+
