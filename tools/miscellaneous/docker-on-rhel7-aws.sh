0db36972aaaa6d68d31a2bf16b18d35896c00f56
KalluruS <>
2021-06-02 16:01:17 +0530

    ss

--- tools/miscellaneous/docker-on-rhel7-aws.sh
+++ tools/miscellaneous/docker-on-rhel7-aws.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+echo '[docker-ce-stable]
+name=Docker CE Stable - $basearch
+baseurl=https://download.docker.com/linux/centos/$releasever/$basearch/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/centos/gpg
+
+[extras]
+name=CentOS-$releasever - Extras
+baseurl=http://mirror.centos.org/centos/7/extras/x86_64/
+enabled=1
+gpgcheck=0' >/etc/yum.repos.d/docker.repo
+
 yum install docker-ce docker-ce-cli containerd.io -y
+
+systemctl enable docker
+systemctl start docker
+
