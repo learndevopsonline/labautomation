f41e5d838533f5c7e428a4ac4b5ae56a00168e36
KalluruS <>
2021-06-02 15:47:47 +0530

    ss

--- tools/miscellaneous/docker-on-rhel7-aws.sh
+++ tools/miscellaneous/docker-on-rhel7-aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yum install -y yum-utils
-
-
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io -y
