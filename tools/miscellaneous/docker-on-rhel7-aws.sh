e2001d0ec6ea50ba1cffac321f32cefd58927410
KalluruS <>
2021-06-04 13:47:40 +0530

    ss

--- tools/miscellaneous/docker-on-rhel7-aws.sh
+++ tools/miscellaneous/docker-on-rhel7-aws.sh
@@ -11,6 +11,12 @@ gpgkey=https://download.docker.com/linux/centos/gpg
 name=CentOS-$releasever - Extras
 baseurl=http://mirror.centos.org/centos/7/extras/x86_64/
 enabled=1
+gpgcheck=0
+
+[os]
+name=CentOS-$releasever - OS
+baseurl=http://mirror.centos.org/centos/7/os/x86_64/
+enabled=1
 gpgcheck=0' >/etc/yum.repos.d/docker.repo
 
 yum install docker-ce docker-ce-cli containerd.io -y
