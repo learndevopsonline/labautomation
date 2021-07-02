916dab701ed3ce0112bda200580cbf5114818bd8
r-devops <raghuk.vit@gmail.com>
2021-07-02 05:50:29 +0530

    Update install.sh

--- tools/terraform/install.sh
+++ tools/terraform/install.sh
@@ -4,7 +4,7 @@ URL=$(curl https://www.terraform.io/downloads.html  | grep 64-bit | grep linux_a
 FILE=$(echo $URL | awk -F / '{print $NF}')
 curl -s -L -J -O $URL 
 sudo yum install unzip -y 
-unzip $FILE -d /bin
+unzip -o $FILE -d /bin
 if [ $? -eq 0 ]; then 
   echo -e "\e[31m SUCCESS\e[0m"
 fi 
