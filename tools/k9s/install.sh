5a39302544d7146a57d3b6ebdedc1daed60360fe
KalluruS <>
2021-08-09 06:15:00 +0530

    ss

--- tools/k9s/install.sh
+++ tools/k9s/install.sh
@@ -2,5 +2,5 @@
 
 cd /tmp
 VERSION=$(curl -s https://github.com/derailed/k9s/releases | grep 'Release v' | head -1 | sed -e 's|<h1>||' -e 's|</h1>||' | awk '{print $2}')
-curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_${VERSION}_Linux_x86_64.tar.gz | tar -xz
+curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar -xz
 mv k9s /bin
