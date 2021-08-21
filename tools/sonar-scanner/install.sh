8f431efd19c3c9bd3a79749c9e3f6cd6b5b6a47f
KalluruS <>
2021-08-21 08:24:12 +0530

    ss

--- tools/sonar-scanner/install.sh
+++ tools/sonar-scanner/install.sh
@@ -6,5 +6,6 @@ unzip sonar-scanner-cli-4.5.0.2216-linux.zip
 rm -rf sonar
 mv sonar-scanner-4.5.0.2216-linux sonar 
 ln -s /opt/sonar/bin/sonar-scanner /bin/sonar-scanner 
-curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/sonar-scanner/sonar-quality-gate.sh >/bin/sonar-quality-gate.sh
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/sonar-scanner/sonar-quality-gate >/bin/sonar-quality-gate.sh
 chmod +x /bin/sonar-quality-gate.sh
+
