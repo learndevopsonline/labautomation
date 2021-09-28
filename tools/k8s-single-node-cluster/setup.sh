7de50beda65e3a17f13a6cb4e4ae6b35dfd5441b
KalluruS <>
2021-09-28 07:47:08 +0530

    ss

--- tools/k8s-single-node-cluster/setup.sh
+++ tools/k8s-single-node-cluster/setup.sh
@@ -14,5 +14,6 @@ mkdir -p ~/.kube
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/terraform/install.sh | sudo bash
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/k8-client-stack/install.sh | sudo bash
 rm -rf .terraform*
+cd /tmp/labautomation/tools/k8s-single-node-cluster
 terraform init
 terraform apply -auto-approve
