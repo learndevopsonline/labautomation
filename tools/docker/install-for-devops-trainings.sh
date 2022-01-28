3094559031a2ae99d7cd4a2948996f0dd1d0488f
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-28 09:22:37 +0530

    ss

--- tools/docker/install-for-devops-trainings.sh
+++ tools/docker/install-for-devops-trainings.sh
@@ -5,9 +5,11 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi 
 
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io -y
+#yum install -y yum-utils device-mapper-persistent-data lvm2
+#yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#yum install docker-ce docker-ce-cli containerd.io -y
+
+curl -s https://get.docker.com | bash
 
 ## Group add 
 id centos &>/dev/null
