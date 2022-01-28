f344875f806a9eb8880a00625608ebc92f12620e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-28 09:23:56 +0530

    ss

--- tools/docker-stack/install.sh
+++ tools/docker-stack/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 echo -e "\e[32m Installing Docker \e[0m"
-curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install-docker-ce.sh | sudo bash 
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install-for-docker-training.sh | sudo bash
 
 echo -e "\e[32m Installing Docker Compose \e[0m"
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker-compose/install.sh | sudo bash 
