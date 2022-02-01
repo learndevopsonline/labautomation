adb8e01e9e238a2137e6f2966f7d7cf021846640
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-01 14:42:33 +0530

    ss

--- tools/terraform/install.sh
+++ tools/terraform/install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash 
 
-URL=$(curl https://www.terraform.io/downloads.html  | grep 64-bit | grep linux_amd64.zip | awk -F \" '{print $2}')
-FILE=$(echo $URL | awk -F / '{print $NF}')
-curl -s -L -J -O $URL 
-sudo yum install unzip -y 
-unzip -o $FILE -d /bin
-if [ $? -eq 0 ]; then 
-  echo -e "\e[31m SUCCESS\e[0m"
-fi 
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
