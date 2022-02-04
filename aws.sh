9e9db7bc60967eeafffb4e41f31205ac6d371f31
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-04 11:05:25 +0530

    ss

--- aws.sh
+++ aws.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export LOG=/tmp/awsauto.log
+rm -f $LOG
+
 echo -e "\n\e[1;33m You can find all the scripts in following location\e[0m\nhttps://github.com/linuxautomations/labautomation/tree/master/aws\n"
 
 curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
@@ -16,7 +19,7 @@ fi
 
 echo -e "${Y}>>>>> Select a AWS Operation to Perform${N}"
 export PS3="Select Action> "
-select action in `ls -1 /tmp/labautomation/aws | sed -e 's/.sh$//'`; do
+select action in `ls -1 /tmp/labautomation/aws/*.sh | sed -e 's/.sh$//'`; do
 		echo -e "\e[1;33m★★★ Performing $action ★★★\e[0m"
 		sh /tmp/labautomation/aws/$action.sh
 		break
--- aws/ec2-spot.sh
+++ aws/ec2-spot.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo spot
+source /tmp/labautomation/aws/common
+
+AMI=$(FIND_AMI)
+
+CHOOSE_INSTANCE_TYPE
+ENTER_INSTANCE_NAME
+CHOOSE_SG_ID
+
+aws ec2 run-instances --image-id ${AMI} --instance-type ${INSTANCE_TYPE} --output text --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${INSTANCE_NAME}}]"  --instance-market-options "MarketType=spot,SpotOptions={InstanceInterruptionBehavior=stop,SpotInstanceType=persistent}" --security-group-ids "${SG_ID}"  &>>$LOG
