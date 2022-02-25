42c1e9bdf70860f64da3bf03d00cff7a70094304
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-25 17:03:23 +0530

    ss

--- tools/sonarqube/install.sh
+++ tools/sonarqube/install.sh
@@ -8,8 +8,9 @@ fi
 yum install epel-release -y
 yum install java-11-openjdk unzip -y
 #URL=$(curl -s https://www.sonarqube.org/downloads/ | grep 'Community Edition' | grep sonarqube-|head -1  | sed -e "s/>/ /g"|xargs -n 1  | grep ^href | awk -F = '{print $2}')
-VERSION=$(curl -s https://binaries.sonarsource.com/Distribution/sonarqube/ | grep '.zip<' | tail -1 | sed -e 's/</ /g'  | xargs -n1 | grep ^href | sed -e 's/href=sonarqube-//' -e 's/.zip>//')
-URL="https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${VERSION}.zip"
+#VERSION=$(curl -s https://binaries.sonarsource.com/Distribution/sonarqube/ | grep '.zip<' | tail -1 | sed -e 's/</ /g'  | xargs -n1 | grep ^href | sed -e 's/href=sonarqube-//' -e 's/.zip>//')
+#URL="https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-${VERSION}.zip"
+URL=$(curl -s -L https://www.sonarqube.org/downloads/ | grep zip  | grep 'Community Edition' | grep 'sonarqube-8' | head -1  | awk -F '"' '{print $2}')
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
 FOLDERNAME=$(echo $FILENAME | sed -e 's/.zip//g')
 
