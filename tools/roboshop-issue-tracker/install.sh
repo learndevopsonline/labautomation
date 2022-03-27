97b99c46f82556c8a64686263ea42531238360ec
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-27 11:23:43 +0530

    ss

--- tools/roboshop-issue-tracker/install.sh
+++ tools/roboshop-issue-tracker/install.sh
@@ -43,7 +43,7 @@ echo -e "\e[1m Checking Nginx Configuration\e[0m"
 DLIM1
 for component in cart catalogue user shipping payment ; do
   [ "$component" == "cart" -o "$component" == "user" ] && TAB="\t"
-  echo -n -e "Checking $COMPONENT Config.. \t${TAB}"
+  echo -n -e "Checking $component Config.. \t${TAB}"
   OUT=$(grep $component /etc/nginx/default.d/roboshop.conf | xargs -n1 | grep ^http | awk -F '[:,/]' '{print $4}')
   if [ "$OUT" != "localhost" ]; then
     echo -e "\e[1;32m Config Found\e[0m - \e[1m Will Check Further for this component\e[0m"
