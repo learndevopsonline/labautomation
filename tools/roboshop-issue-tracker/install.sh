3d8d01ac98628c7b7ced58fa90bc751d5d4d61a3
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-27 07:30:31 +0530

    ss

--- tools/roboshop-issue-tracker/install.sh
+++ tools/roboshop-issue-tracker/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\e[1;33m This script helps you to Check the Issues of RoboShop\e[0m"
-echo -e "\e[1m Note: This script is still under development and does not have all the cases ready yet\[0m"
+echo -e "\n\n\e[1;33m This script helps you to Check the Issues of RoboShop\e[0m"
+echo -e "\e[1m Note: This script is still under development and does not have all the cases ready yet\e[0m"
 
-echo -e "This script runs only in FRONTEND, Hence if you are sure that you are \nrunning under frontend server then proceed.."
+echo -e "\nThis script runs only in FRONTEND, Hence if you are sure that you are \nrunning under frontend server then proceed.."
 read -p 'Proceed [Y|n]: ' action
 ACTION=$(echo $action| cut -c -1 | tr [a-z] [A-Z])
 if [ "$ACTION" == "Y" ]; then
