4491802e866ee576f8d450014deb337f61ab911c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-18 12:54:26 +0530

    ss

--- tools/roboshop-issue-tracker/catalogue.bash
+++ tools/roboshop-issue-tracker/catalogue.bash
@@ -4,11 +4,11 @@ source /tmp/functions
 #
 echo -e "Restarting Catalogue"
 systemctl restart catalogue
+STAT "Restart Catalogue"
 sleep 5
-#STAT "Restart Catalogue"
 
 echo -e "Checking MongoDB Status"
-curl http://127.0.0.1:8080/health | jq .mongo
+curl localhost:8080/health | jq .mongo
 DBSTAT=`curl -s localhost:8080/health | jq .mongo`
 if [ $DBSTAT == "true" ]; then
   wB "Catalogue Connecting to MongoDB is \e[32mSUCCESS"
