59c764f67f39d324412a00983bb5648c27b5959d
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-18 12:16:43 +0530

    ss

--- tools/roboshop-issue-tracker/catalogue.bash
+++ tools/roboshop-issue-tracker/catalogue.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/roboshop-issue-tracker/functions >/tmp/functions
+source /tmp/functions
+
 echo -e "Restarting Catalogue"
 systemctl restart catalogue
 DBSTAT=$(curl -s localhost:8080/health | jq .mongo)
