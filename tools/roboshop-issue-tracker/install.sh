eb772f56d1ce74616b25ce57ad9fbf29a1532607
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-19 12:29:31 +0530

    ss

--- tools/roboshop-issue-tracker/install.sh
+++ tools/roboshop-issue-tracker/install.sh
@@ -2,11 +2,14 @@
 
 source $(dirname $0)/functions
 
-echo -e "\n\n\e[1;33m This script helps you to Check the Issues of RoboShop\e[0m"
-echo -e "\e[1m Note: This script is still under development and does not have all the cases ready yet.\e[0m"
+NLPRINT
+NLPRINT
+RBPRINT "This script helps you to Check the Issues of RoboShop"
+BPRINT  "Note: This script is still under development and does not have all the cases ready yet."
 
-echo -e "\nThis script runs only in FRONTEND, Hence if you are sure that you are \nrunning under frontend server then proceed.."
+PRINT   "\nThis script runs only in FRONTEND, Hence if you are sure that you are \nrunning under frontend server then proceed.."
 read -p 'Proceed [Y|n]: ' action
+
 ACTION=$(echo $action| cut -c -1 | tr [a-z] [A-Z])
 if [ "$ACTION" == "Y" ]; then
   if [ ! -f /etc/nginx/default.d/roboshop.conf ]; then
@@ -16,6 +19,55 @@ if [ "$ACTION" == "Y" ]; then
 else
   exit
 fi
+
+## Component - 1 - Scenario 1
+
+DLIM
+echo -e "\e[1m Checking Nginx Configuration\e[0m"
+DLIM1
+
+for component in cart catalogue user shipping payment ; do
+  [ "$component" == "cart" -o "$component" == "user" ] && TAB="\t"
+  echo -n -e "Checking $component Config.. \t${TAB}"
+  OUT=$(grep $component /etc/nginx/default.d/roboshop.conf | xargs -n1 | grep ^http | awk -F '[:,/]' '{print $4}')
+  if [ "$OUT" != "localhost" ]; then
+    echo -e "\e[1;32m Config Found\e[0m - \e[1m Will Check Further for this component\e[0m"
+    FINAL="$FINAL $component"
+  else
+    echo -e "\e[1;31m Config Not Found\e[0m - \e[1m Will Not Check Further for this component\e[0m"
+  fi
+  unset TAB
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exit
 
 ## Cases
 RHEAD() {
@@ -32,21 +84,8 @@ R3() {
 }
 
 ## Report Configuration
-DLIM
-echo -e "\e[1m Checking Nginx Configuration\e[0m"
-DLIM1
-for component in cart catalogue user shipping payment ; do
-  [ "$component" == "cart" -o "$component" == "user" ] && TAB="\t"
-  echo -n -e "Checking $component Config.. \t${TAB}"
-  OUT=$(grep $component /etc/nginx/default.d/roboshop.conf | xargs -n1 | grep ^http | awk -F '[:,/]' '{print $4}')
-  if [ "$OUT" != "localhost" ]; then
-    echo -e "\e[1;32m Config Found\e[0m - \e[1m Will Check Further for this component\e[0m"
-    FINAL="$FINAL $component"
-  else
-    echo -e "\e[1;31m Config Not Found\e[0m - \e[1m Will Not Check Further for this component\e[0m"
-  fi
-  unset TAB
-done
+
+
 
 for component in ${FINAL}; do
   DLIM
