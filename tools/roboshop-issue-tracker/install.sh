c0fca1ea5a34f84a35a48044760c93fa305d1ec6
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-19 23:05:04 +0530

    ss

--- tools/roboshop-issue-tracker/install.sh
+++ tools/roboshop-issue-tracker/install.sh
@@ -125,7 +125,7 @@ else
   else
     EXIT "\nMongoDB need to have schema for the application to work, So follow documentation and load the schema"
   fi
-  CONCLUDE "👍 All good with MongoDB"
+  CONCLUDE "🥳 👍 All good with MongoDB"
 fi
 
 
