189f3558911eac9881ec8730af8b99c1a3402e62
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2022-07-15 06:19:23 +0530

    Update install.sh

--- tools/elk/install.sh
+++ tools/elk/install.sh
@@ -11,7 +11,7 @@ name=Elasticsearch repository for 7.x packages
 baseurl=https://artifacts.elastic.co/packages/7.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-enabled=0
+enabled=1
 autorefresh=1
 type=rpm-md' > /etc/yum.repos.d/elastic.repo
 Stat $? "Setup Yum Repo"
