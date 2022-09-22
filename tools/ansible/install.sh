f25713d9cd11bbf0fb8fa47d716c62e9f8f3c976
Raghu K <raghuk.vit@gmail.com>
2022-09-22 06:32:38 +0530

    ss

--- tools/ansible/install.sh
+++ tools/ansible/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash 
 
-yum install python3-pip -y
-pip3 install pip --upgrade
-pip3 install ansible
+source /tmp/common-functions.sh
+
+if [ "$ELV" == "el7" ]; then
+  yum install python3-pip -y
+  pip3 install pip --upgrade
+  pip3 install ansible
+elif [ "$ELV" == "el8" ]; then
+  yum install python39-devel -y
+  pip3.9 install ansible
+fi
