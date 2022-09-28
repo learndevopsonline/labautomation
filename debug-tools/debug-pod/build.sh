4d521928e07c3e7015c3024f963d997e215e6fe7
Raghu K <raghuk.vit@gmail.com>
2022-09-28 16:35:06 +0530

    ss

--- debug-tools/debug-pod/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-#docker build -t ghcr.io/r-devops/debug .
-docker build -t rkalluru/debug:centos7 .
-#docker push ghcr.io/r-devops/debug
-docker push rkalluru/debug:centos7
