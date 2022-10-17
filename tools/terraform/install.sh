b3bf8d950806f4e94e329114e33f0b610766bb6e
Raghu K <raghuk.vit@gmail.com>
2022-10-17 14:31:43 +0530

    ss

--- tools/terraform/install.sh
+++ tools/terraform/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash 
 
+curl -s -L https://raw.githubusercontent.com/linuxautomations/labautomation/master/dry/common-functions.sh >/tmp/common-functions.sh
+source /tmp/common-functions.sh
 yum install -y yum-utils
 case $OSVENDOR in
   CentOS)
     yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
   ;;
-  Amazon Linux)
+  "Amazon Linux")
     yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
   ;;
   *)
