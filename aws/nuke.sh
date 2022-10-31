f0eddb8f322d4c929a05cccf2d6abcfef85e1693
Raghu K <raghuk.vit@gmail.com>
2022-10-31 21:12:20 +0530

    ss

---
+++ aws/nuke.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# DANGER : This script is going to delete all the resources in AWS account.
+# Note: Prefer to run in cloud shell of aws.
+
+## Pre-Setup Steps
+# 1. Configure AWS CLI with Credentials. Only this credentials user will be discarded
+# 2. clone this repo and run this script
+#     git clone https://github.com/linuxautomations/labautomation
+#     cd  labautomation
+#     bash aws/nuke.sh
+#
