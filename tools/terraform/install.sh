ff9a51c1d802eb0abcbc6b66c6d00470d45b3cb2
Raghu K <raghuk.vit@gmail.com>
2022-10-31 06:40:58 +0530

    ss

--- tools/terraform/install.sh
+++ tools/terraform/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 #source ../$(pwd  | awk -F 'labautomation/' '{print $2}')/dry/common-functions.sh
+if [ -f dry/common-functions.sh ]; then
+  source dry/common-functions.sh
+fi
 
-dirname `pwd`
-
-exit 1
 curl -s -L https://raw.githubusercontent.com/linuxautomations/labautomation/master/dry/common-functions.sh >/tmp/common-functions.sh
 source /tmp/common-functions.sh
 yum install -y yum-utils
