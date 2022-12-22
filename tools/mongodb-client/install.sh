2c4e140c5647856fd83836ce145f1bab512dac32
Raghu K <raghuk.vit@gmail.com>
2022-12-22 18:25:10 +0530

    ss

---
+++ tools/mongodb-client/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongo.repo
+
+yum install mongocli -y
