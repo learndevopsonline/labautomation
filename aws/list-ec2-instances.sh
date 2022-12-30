a93b38a1d1307a7deb8418099b6295d399ba4f17
Raghu K <raghuk.vit@gmail.com>
2022-12-30 12:10:29 +0530

    ss

--- aws/list-ec2-instances.sh
+++ aws/list-ec2-instances.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress,State:State}' --output table
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress,State:State.Name}' --output table
+
