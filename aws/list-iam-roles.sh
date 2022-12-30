4523771fb179989da9a3f406cfa25ed1077d7683
Raghu K <raghuk.vit@gmail.com>
2022-12-30 12:31:54 +0530

    ss

--- aws/list-iam-roles.sh
+++ aws/list-iam-roles.sh
@@ -1,2 +1,11 @@
 aws iam list-roles --query 'Roles[*].{Name:RoleName}' --output table
 
+echo -e "Enter Role Name to Describe: "
+read -p '' role_name
+
+if [ -z "$role_name" ]; then
+  exit
+fi
+
+aws iam get-role --role-name $role_name --output table
+
