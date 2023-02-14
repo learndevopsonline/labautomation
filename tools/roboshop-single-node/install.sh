e47158439b4853f188b3890e6efeb69261d47a05
Raghu K <raghuk.vit@gmail.com>
2023-02-14 22:08:50 +0530

    ss

--- tools/roboshop-single-node/install.sh
+++ tools/roboshop-single-node/install.sh
@@ -234,5 +234,31 @@ systemctl start user
 mongo --host localhost </catalogue/schema/catalogue.js
 mongo --host localhost </user/schema/user.js
 
+yum install nginx -y
+rm -rf /usr/share/nginx/html/*
 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+echo '
+proxy_http_version 1.1;
+location /images/ {
+  expires 5s;
+  root   /usr/share/nginx/html;
+  try_files $uri /images/placeholder.jpg;
+}
+location /api/catalogue/ { proxy_pass http://localhost:8081/; }
+location /api/user/ { proxy_pass http://localhost:8082/; }
+location /api/cart/ { proxy_pass http://localhost:8083/; }
+location /api/shipping/ { proxy_pass http://localhost:8080/; }
+location /api/payment/ { proxy_pass http://localhost:8084/; }
+
+location /health {
+  stub_status on;
+  access_log off;
+}
+' >/etc/nginx/default.d/roboshop.conf
+systemctl restart nginx
+systemctl enable nginx
 
