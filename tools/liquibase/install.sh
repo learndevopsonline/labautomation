dc4b283532cd6f0b0276e41f509fcf96112ce25f
Raghu K <raghuk.vit@gmail.com>
2023-03-05 08:12:38 +0530

    ss

--- tools/liquibase/install.sh
+++ tools/liquibase/install.sh
@@ -3,5 +3,9 @@
 VERSION=$(curl -s -L https://github.com/liquibase/liquibase/tags | grep 'id="toggle-commit-' | head -1  | xargs -n 1  | grep 'id=toggle-commit-' | awk -F - '{print $NF}' | sed -e 's/v//')
 
 cd /tmp/
-curl -L -O https://github.com/liquibase/liquibase/releases/download/v$VERSION/liquibase-$VERSION.tar.gz
+curl -s -L -O https://github.com/liquibase/liquibase/releases/download/v$VERSION/liquibase-$VERSION.tar.gz
+rm -rf /opt/liquibase
+mkdir -p /opt/liquibase
+cd /opt/liquibase
+tar -xf /tmp/liquibase-$VERSION.tar.gz ; rm -f liquibase-$VERSION.tar.gz
 
