2d054ef35c5f17ca8619fcdddcbf6003b36ba8bc
Raghu K <raghuk.vit@gmail.com>
2023-03-05 08:22:48 +0530

    ss

--- tools/liquibase/install.sh
+++ tools/liquibase/install.sh
@@ -2,9 +2,12 @@
 
 VERSION=$(curl -s -L https://github.com/liquibase/liquibase/tags | grep 'id="toggle-commit-' | head -1  | xargs -n 1  | grep 'id=toggle-commit-' | awk -F - '{print $NF}' | sed -e 's/v//')
 
-yum install java-11-openjdk -y
+yum install java-11-openjdk -y &>/tmp/liquibase.log
+Stat $? "Install Java"
 cd /tmp/
-curl -s -L -O https://github.com/liquibase/liquibase/releases/download/v$VERSION/liquibase-$VERSION.tar.gz
+curl -f -s -L -O https://github.com/liquibase/liquibase/releases/download/v$VERSION/liquibase-$VERSION.tar.gz
+Stat $? "Download Liquibase"
+
 rm -rf /opt/liquibase
 mkdir -p /opt/liquibase
 cd /opt/liquibase
