b2804ed8d7bf5a8e0bff65e7c98b7a8095432ae4
Raghu K <raghuk.vit@gmail.com>
2023-03-17 18:36:37 +0530

    ss

--- dry/common-functions.sh
+++ dry/common-functions.sh
@@ -12,6 +12,7 @@ LU="\e[2m"
 N="\e[0m"
 
 #ELV=$(rpm -q basesystem |sed -e 's/\./ /g' |xargs -n 1|grep ^el)
+ELV=$(rpm -qi basesystem | grep Release  | awk -F . '{print $NF}')
 export OSVENDOR=$(rpm -qi basesystem  | grep Vendor | awk -F : '{print $NF}' | sed -e 's/^ //')
 
 ## Common Functions
