f4afdd835155acaf12ba74853b13232313491d7a
Raghu K <raghuk.vit@gmail.com>
2023-03-29 17:08:17 +0530

    ss

--- tools/roboshop-issue-tracker/install.sh
+++ tools/roboshop-issue-tracker/install.sh
@@ -11,7 +11,10 @@ chatgpt_print FRONTEND: Checking if the nginx is installed
 command_print "yum list installed | grep nginx.x"
 
 yum list installed | grep nginx.x
-Stat $? "\nNginx is installed"
+echo
+Stat $? "Nginx is installed"
+
+
 
 
 
