57105c8d37b3463c1ec2c23cb1966141d2dbaeb0
Raghu K <raghuk.vit@gmail.com>
2023-04-04 11:45:50 +0530

    ss

--- aws/list-security-groups.sh
+++ aws/list-security-groups.sh
@@ -2,4 +2,4 @@ aws ec2   describe-security-groups --query "SecurityGroups[*].{ID:GroupId, Name:
 
 read -p 'Enter security group ID: ' id
 
-aws ec2 describe-security-group-rules --filter Name="group-id",Values="${id}" --query "SecurityGroupRules[*].{from: FromPort, to: ToPort, cidr:CidrIpv4}" --output table
+aws ec2 describe-security-group-rules --filter Name="group-id",Values="${id}" --query "SecurityGroupRules[*].{from: FromPort, to: ToPort, cidr:CidrIpv4,ingress: IsEgress}" --output table
