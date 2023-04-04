91c287cb19ce33c710848804f1cf043f08ab5b4a
Raghu K <raghuk.vit@gmail.com>
2023-04-04 11:39:33 +0530

    ss

--- aws/list-security-groups.sh
+++ aws/list-security-groups.sh
@@ -1,1 +1,5 @@
-aws ec2   describe-security-groups --query "SecurityGroups[*].[GroupName]" --output table
+aws ec2   describe-security-groups --query "SecurityGroups[*].{ID:GroupId, Name: GroupName}" --output table
+
+read -p 'Enter security group ID: ' id
+
+aws ec2 describe-security-groups --group-ids ${id} --output json
