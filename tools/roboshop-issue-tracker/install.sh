82165051fc6a4be749a7c90bca4ea1b4f0e71d6e
Raghu K <raghuk.vit@gmail.com>
2023-04-13 12:50:32 +0530

    ss

--- tools/roboshop-issue-tracker/install.sh
+++ tools/roboshop-issue-tracker/install.sh
@@ -168,11 +168,11 @@ else
   StatP 0 "REDIS listen address is configured"
 fi
 
-#chatgpt_print "USER: Checking if user is able to reach Redis Server or not"
-#command_print "nc -w 5 -z $REDIS_IP 6379"
-#remote_command $USE_IP "nc -w 5 -z $REDIS_IP 6379"
-#Stat $? "User server able to connect to Redis server"
-#
+chatgpt_print "USER: Checking if user is able to reach Redis Server or not"
+command_print "nc -w 5 -z $REDIS_IP 6379"
+remote_command $USE_IP "nc -w 5 -z $REDIS_IP 6379"
+Stat $? "User server able to connect to Redis server"
+
 ## Finding Cart Server
 #command_print "cat /etc/nginx/default.d/roboshop.conf  | grep cart  | xargs -n1 | grep ^http | sed -e 's|http://||' | awk -F : '{print \$1}'"
 #
