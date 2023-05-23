05681f1e979096add476db35c26cfe7df1ed6ee1
Raghu K <raghuk.vit@gmail.com>
2023-05-23 08:59:40 +0530

    ss

--- aws/create-roboshop-instances.sh
+++ aws/create-roboshop-instances.sh
@@ -2,6 +2,7 @@
 
 ##### Change these values ###
 ZONE_ID=$(aws route53 list-hosted-zones --query "HostedZones[*].{ID:Id,Name:Name,Private:Config.PrivateZone}" --output text | awk '{print $1}' | awk -F / '{print $3}')
+DOMAIN_NAME=$(aws route53 list-hosted-zones --query "HostedZones[*].{ID:Id,Name:Name,Private:Config.PrivateZone}" --output text | awk '{print $2}' | sed -e 's/.$//')
 SG_NAME="allow-all"
 #ENV="dev"
 #############################
@@ -15,7 +16,7 @@ create_ec2() {
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
       --security-group-ids ${SGID} | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" /tmp/labautomation/aws/route53.json >/tmp/record.json
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN_NAME/${DOMAIN_NAME}/" /tmp/labautomation/aws/route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
@@ -35,6 +36,6 @@ fi
 
 
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
-  COMPONENT="${env}-${component}"
+  COMPONENT="${component}"
   create_ec2
 done
