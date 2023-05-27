eb1328290e49274257dae7cd752877b6196cf384
Raghu K <raghuk.vit@gmail.com>
2023-05-27 12:55:37 +0530

    ss

--- aws/nuke.sh
+++ aws/nuke.sh
@@ -23,4 +23,14 @@ for user in ${ALL_USERS}; do
   aws iam delete-user --user-name $user
 done
 
+ROLES=$(aws iam list-roles --query 'Roles[].RoleName' --output text|xargs -n1 | grep -Ev '^AWS|cross_role|main_role|OrganizationAccountAccessRole|workstation')
 
+for role in ${ROLES}; do
+  aws iam delete-role --role-name $role
+done
+
+POLICIES=$(aws iam list-policies --scope Local --query "Policies[].Arn" --output text   | xargs -n1)
+
+for policy in ${POLICIES}; do
+  aws iam delete-policy --policy-arn $policy
+done
