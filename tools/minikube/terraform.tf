8325674e8f1c46d94b8c288e49da42340e4c191e
Raghu K <raghuk.vit@gmail.com>
2023-06-21 07:25:23 +0530

    ss

--- tools/minikube/terraform.tf
+++ tools/minikube/terraform.tf
@@ -33,28 +33,28 @@ module "vpc" {
   }
 }
 
-//module "minikube" {
-//  source = "github.com/scholzj/terraform-aws-minikube"
-//
-//  aws_region    = "us-east-1"
-//  cluster_name  = "minikube"
-//  aws_instance_type = "t3.medium"
-//  ssh_public_key = "~/.ssh/id_rsa.pub"
-//  aws_subnet_id = element(lookup(module.vpc, "public_subnets", null), 0)
-//  ami_image_id = data.aws_ami.ami.id
-//  hosted_zone = "my-domain.com"
-//  hosted_zone_private = false
-//
-//  tags = {
-//    Application = "Minikube"
-//  }
-//
-//  addons = [
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-//  ]
-//}
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/.ssh/id_rsa.pub"
+  aws_subnet_id = element(lookup(module.vpc, "public_subnets", null), 0)
+  ami_image_id = data.aws_ami.ami.id
+  hosted_zone =
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
 
 
