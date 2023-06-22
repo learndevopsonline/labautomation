cca61e1364ad6d00cbd7ab884d08032a4b07cda9
Raghu K <raghuk.vit@gmail.com>
2023-06-22 06:52:39 +0530

    ss

--- tools/minikube/create.sh
+++ tools/minikube/create.sh
@@ -18,6 +18,10 @@ if [ ! -f "~/.ssh/id_rsa.pub" ]; then
   cat /dev/zero | ssh-keygen -q -N ""
 fi
 
+echo "Ensure you open the following URL and subscribe. Wait for Subscription to Complete and Press Enter to Continue"
+echo "https://aws.amazon.com/marketplace/pp/prodview-foff247vr2zfw?ref_=aws-mp-console-subscription-detail"
+read -p ""
+
 rm -rf .terraform*
 
 terraform init
