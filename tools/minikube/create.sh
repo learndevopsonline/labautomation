2c51a558189d9517a7bfc89c1e29dd531df55ea3
Raghu K <raghuk.vit@gmail.com>
2023-06-22 06:58:56 +0530

    ss

--- tools/minikube/create.sh
+++ tools/minikube/create.sh
@@ -18,6 +18,9 @@ if [ ! -f "~/.ssh/id_rsa.pub" ]; then
   cat /dev/zero | ssh-keygen -q -N ""
 fi
 
+echo
+echo
+echo
 echo "Ensure you open the following URL and subscribe. Wait for Subscription to Complete and Press Enter to Continue"
 echo "https://aws.amazon.com/marketplace/pp/prodview-foff247vr2zfw?ref_=aws-mp-console-subscription-detail"
 read -p ""
