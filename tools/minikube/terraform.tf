35099c3c91fdcd70144b0203eb6724aec2b97b85
Raghu K <raghuk.vit@gmail.com>
2023-06-22 07:05:11 +0530

    ss

--- tools/minikube/terraform.tf
+++ tools/minikube/terraform.tf
@@ -57,6 +57,6 @@ module "minikube" {
 }
 
 output "kube_config" {
-  value = "Copy Kubernetes Configuration File From MiniKube\nExecute the following command\nsudo scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig /tmp/kubeconfig"
+  value = "Copy Kubernetes Configuration File From MiniKube\nExecute the following command\n\n\nsudo scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig /tmp/kubeconfig\nsudo chmod ugo+r /tmp/kubeconfig\n\n\n"
 }
 
