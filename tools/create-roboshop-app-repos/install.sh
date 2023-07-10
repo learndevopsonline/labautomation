7d80fbae1b4d315550d08436f6baf991cd81885c
Raghu K <raghuk.vit@gmail.com>
2023-07-10 08:01:43 +0530

    ss

--- tools/create-roboshop-app-repos/install.sh
+++ tools/create-roboshop-app-repos/install.sh
@@ -7,7 +7,7 @@ for repo in catalogue user cart shipping payment frontend dispatch ; do
   git clone https://github.com/roboshop-devops-project-v1/$repo
   cd /home/centos
   gh repo create $username/$repo --public --clone
-  cd /hom/centos/$repo
+  cd /home/centos/$repo
   cp -r /tmp/$repo/* .
   break
 done
