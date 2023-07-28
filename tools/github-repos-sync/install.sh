ef49b11e0e5a810d376956ce6a72eef5c58e91c9
Raghu K <raghuk.vit@gmail.com>
2023-07-28 10:56:07 +0530

    ss

---
+++ tools/github-repos-sync/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p /opt/github-repos
+cd /opt/github-repos
+
+for repo in `gh repo list | grep -Ev '^Showing|^$' | awk '{print $1}' | awk -F / '{print $2}'`; do
+
+  if [ ! -d "$repo" ]; then
+    gh repo clone $repo
+    continue
+  fi
+
+  cd $repo
+  git pull
+  cd ..
+done
+
+sed -i -e '/alias cfr/ d' /etc/environment
+echo 'alias cfr="git add -A ; git commit -m "Changes from Raghu"; git push' >>/etc/environment
