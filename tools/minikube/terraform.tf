64adb1b4bf83d01f7de19b1927dc34e2699515c0
Raghu K <raghuk.vit@gmail.com>
2023-08-10 21:18:35 +0530

    ss

--- tools/minikube/terraform.tf
+++ tools/minikube/terraform.tf
@@ -4,12 +4,6 @@ terraform {
   }
 }
 
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
-}
-
 data "external" "zone" {
 program = ["bash", "${path.root}/route53"]
 }
