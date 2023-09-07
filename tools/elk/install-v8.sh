8549c6a66470a622b5db10f0de3e25e622e42a45
Raghu K <raghuk.vit@gmail.com>
2023-09-07 07:23:31 +0530

    ss

---
+++ tools/elk/install-v8.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
+#source /root/scripts/common-functions.sh
+source /tmp/common-functions.sh
+
+echo '[elasticsearch]
+name=Elasticsearch repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+' >/etc/yum.repos.d/elastic.repo
+
+yum install elasticsearch -y | tee -a /opt/elastic.log
+
+
+systemctl enable elasticsearch &>>/tmp/elastic.log
+systemctl start elasticsearch &>>/tmp/elastic.log
+
+yum install kibana -y &>>/tmp/elastic.log
+Stat $? "Install Kibana"
+
+
+systemctl enable kibana &>>/tmp/elastic.log
+systemctl start kibana &>>/tmp/elastic.log
+Stat $? "Start Kibana"
+
+yum install logstash -y &>>/tmp/elastic.log
+Stat $? "Install LogStash"
+
+echo 'input {
+  beats {
+    port => 5044
+  }
+}
+
+output {
+  elasticsearch {
+    hosts => ["http://localhost:9200"]
+    index => "%{[@metadata][beat]}-%{[@metadata][version]}"
+  }
+}' >/etc/logstash/conf.d/logstash.conf
+
+systemctl enable logstash &>>/tmp/elastic.log
+systemctl start logstash &>>/tmp/elastic.log
+Stat $? "Start Logstash"
+
+yum install nginx -y &>/dev/null
+Stat $? "Install Nginx"
+
+
+curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/elk/http-proxy.conf >/etc/nginx/nginx.conf
+systemctl enable nginx
+systemctl start nginx
+Stat $? "Start Kibana"
+
