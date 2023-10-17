d21af433b8b5b1a186d0866c8cba775d86b4b68b
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-17 08:12:59 +0530

    ss

---
+++ tools/artifactory/install.sh
@@ -0,0 +1,13 @@
+echo '[Artifactory]
+name=Artifactory
+baseurl=https://releases.jfrog.io/artifactory/artifactory-rpms/
+enabled=1
+gpgcheck=0
+gpgkey=https://releases.jfrog.io/artifactory/artifactory-rpms/repodata/repomd.xml.key
+repo_gpgcheck=0' > /etc/yum.repos.d/artifactory.repo
+
+PACK=$(yum list all  --showduplicates| grep Artifactory | grep jfrog-artifactory-oss.x86_64 | tail -1 | awk '{print $1"-"$2}' | sed -e 's/.x86_64//')
+
+yum install $PACK -y
+systemctl enable artifactory
+systemctl start artifactory
