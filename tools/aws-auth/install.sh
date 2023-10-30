bc2b7b7d482c28015471e29072c2e03c78c0acd8
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-30 08:19:24 +0530

    Changes to Minikube

---
+++ tools/aws-auth/install.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/keikoproj/aws-auth/releases/download/v0.5.0/aws-auth_v0.5.0_linux_amd64.tar.gz -o /tmp/aws-auth_v0.5.0_linux_amd64.tar.gz
+cd /tmp
+tar -xf aws-auth_v0.5.0_linux_amd64.tar.gz
