46e1d581acead3d9e0912740e42d35e0effa4e37
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-04 07:32:01 +0530

    Changes to Minikube

---
+++ tools/github-create-repo/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+type gh &>/dev/null
+if [ $? -ne 0 ]; then
+  curl -L https://cli.github.com/packages/rpm/gh-cli.repo >/etc/yum.repos.d/gh.repo
+  yum install gh -y
+fi
+
+gh repo list &>/dev/null
+if [ $? -ne 0 ]; then
+  gh auth login && gh repo list
+fi
+
+read -p 'Enter repository name: ' name
+CDIR=$(pwd)
+
+mkdir local
+cd local
+echo "# $name" >README.md
+git init
+git branch -m main
+git add -A
+git commit -m INIT
+gh repo create $name --public  --source=. --push
+
+
