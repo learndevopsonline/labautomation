13012835b1cebcc9144f543332c7ba66afac72b4
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-11-27 08:07:03 +0530

    Changes to Minikube

--- tools/roboshop-app-repos-import/install.sh
+++ tools/roboshop-app-repos-import/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read -p 'Enter GitHub Username: ' gusername
+
 SOURCE_REPO=https://github.com/roboshop-devops-project-v1
 
 for component in cart catalogue user shipping payment dispatch frontend ; do
@@ -7,5 +9,5 @@ for component in cart catalogue user shipping payment dispatch frontend ; do
   rm -rf $component
   git clone $SOURCE_REPO/$component
   rm -rf /tmp/$component/.git
-  gh repo create my-project --public --source=. --remote=upstream
+  gh repo create $gusername/$component --public --source=. --remote=upstream
 done
