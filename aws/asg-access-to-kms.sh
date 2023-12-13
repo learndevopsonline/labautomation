498c9cf267d2ab96b2fef4296112100395f1c7fa
Raghu K <raghuk.vit@gmail.com>
2023-12-13 06:30:28 +0530

    ss

--- aws/asg-access-to-kms.sh
+++ aws/asg-access-to-kms.sh
@@ -13,7 +13,8 @@ if [ $KC -eq 0 ]; then
   exit 1
 fi
 
-ARN=$(aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g')
+ALIAS=$(aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | awk -F : '{print $NF}')
+ARN=$(aws kms describe-key --key-id $ALIAS --query 'KeyMetadata.Arn' --output text)
 ACCOUNT=$(aws sts get-caller-identity  --query 'Account' --output text)
 
 aws kms create-grant --region us-east-1 --key-id $ARN --grantee-principal arn:aws:iam::$ACCOUNT:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling --operations "Encrypt" "Decrypt" "ReEncryptFrom" "ReEncryptTo" "GenerateDataKey" "GenerateDataKeyWithoutPlaintext" "DescribeKey" "CreateGrant"
