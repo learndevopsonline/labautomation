8694cd45136a0fd0ea74888961a781fcf59d9622
Raghu K <raghuk.vit@gmail.com>
2023-12-25 23:26:41 +0530

    ss

--- tools/python/python-3.12-on-centos-8.sh
+++ tools/python/python-3.12-on-centos-8.sh
@@ -1,9 +1,10 @@
-LINK=$(curl -s -L https://www.python.org/downloads/ | grep 'python/3.12' | grep tar | head -1 | awk -F '"' '{print $4}')
-FILE=$(echo $LINK | awk -F / '{print $NF}')
-DIR=$(echo $FILE | sed -e 's/.tar.xz//')
-cd /tmp
-cd /tmp/Python*
-curl -L -O $LINK
-tar -xf $FILE && rm -f $FILE
-cd $DIR
-ls -l
+#LINK=$(curl -s -L https://www.python.org/downloads/ | grep 'python/3.12' | grep tar | head -1 | awk -F '"' '{print $4}')
+#FILE=$(echo $LINK | awk -F / '{print $NF}')
+#DIR=$(echo $FILE | sed -e 's/.tar.xz//')
+#cd /tmp
+#cd /tmp/Python*
+#curl -L -O $LINK
+#tar -xf $FILE && rm -f $FILE
+#cd $DIR
+#ls -l
+curl -L  https://labautomation-s3.s3.ap-south-1.amazonaws.com/python-3.12.tar.gz  | tar -x
