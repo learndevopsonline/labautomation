dd5c21ff5d87e8e367b5ba7c3becd85b7a18acf3
Raghu K <raghuk.vit@gmail.com>
2023-12-25 23:54:54 +0530

    ss

--- tools/python/python-3.12-on-centos-8.sh
+++ tools/python/python-3.12-on-centos-8.sh
@@ -7,4 +7,8 @@
 #tar -xf $FILE && rm -f $FILE
 #cd $DIR
 #ls -l
+cd /opt
 curl -L  https://labautomation-s3.s3.ap-south-1.amazonaws.com/python-3.12.tar.gz  | tar -x
+rm -f /bin/python &>/dev/null
+ln -s /opt/python/bin/python3 /bin/python
+
