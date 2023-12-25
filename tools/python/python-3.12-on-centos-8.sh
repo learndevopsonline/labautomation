edd683dd1e02a33096b2028457185889e8527928
Raghu K <raghuk.vit@gmail.com>
2023-12-25 22:16:10 +0530

    ss

--- tools/python/python-3.12-on-centos-8.sh
+++ tools/python/python-3.12-on-centos-8.sh
@@ -2,6 +2,7 @@ LINK=$(curl -s -L https://www.python.org/downloads/ | grep 'python/3.12' | grep 
 FILE=$(echo $LINK | awk -F / '{print $NF}')
 DIR=$(echo $FILE | sed -e 's/.tar.xz//')
 cd /tmp
+cd /tmp/Python*
 curl -L -O $LINK
 tar -xf $FILE && rm -f $FILE
 cd $DIR
