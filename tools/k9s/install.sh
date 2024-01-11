dfede474843b49d938f168738097ee32577084a8
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-11 18:29:46 +0530

    ss

--- tools/k9s/install.sh
+++ tools/k9s/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-cd /tmp
 VERSION=$(curl -s https://github.com/derailed/k9s/releases | grep 'Release v' | head -1 | sed -e 's|<h1>||' -e 's|</h1>||' | awk '{print $2}')
-curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar -xz
-mv k9s /bin
+#curl -L https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_Linux_x86_64.tar.gz | tar -xz
+dnf install https://github.com/derailed/k9s/releases/download/${VERSION}/k9s_linux_amd64.rpm -y
+
