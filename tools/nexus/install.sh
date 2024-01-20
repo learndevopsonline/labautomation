cda860aaafadeb7acc48435bd3409ab64bbefb3f
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-20 13:58:06 +0530

    ss

--- tools/nexus/install.sh
+++ tools/nexus/install.sh
@@ -36,7 +36,8 @@ fi
 
 ## Downloading Nexus
 yum install https://kojipkgs.fedoraproject.org/packages/python-html2text/2016.9.19/1.el7/noarch/python2-html2text-2016.9.19-1.el7.noarch.rpm -y &>/dev/null
-URL=$(curl -L -s https://help.sonatype.com/display/NXRM3/Download+Archives+-+Repository+Manager+3 | html2text | grep tar.gz | sed -e 's/>//g' -e 's/<//g' | grep ^http|head -1 | awk '{print $1}')
+#URL=$(curl -L -s https://help.sonatype.com/display/NXRM3/Download+Archives+-+Repository+Manager+3 | html2text | grep tar.gz | sed -e 's/>//g' -e 's/<//g' | grep ^http|head -1 | awk '{print $1}')
+URL="https://download.sonatype.com/nexus/3/nexus-3.64.0-04-unix.tar.gz"
 NEXUSFILE=$(echo $URL | awk -F '/' '{print $NF}')
 NEXUSDIR=$(echo $NEXUSFILE|sed -e 's/-unix.tar.gz//')
 NEXUSFILE="/opt/$NEXUSFILE"
