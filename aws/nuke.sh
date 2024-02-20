18618166d4876f114bba6e3d5769950dba686cba
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2024-02-20 19:07:59 +0530

    NUKE

--- aws/nuke.sh
+++ aws/nuke.sh
@@ -11,6 +11,8 @@
 #     bash aws/nuke.sh
 #
 
+AccountNo=$(aws sts get-caller-identity | jq .Account |xargs)
+
 # Delete Instances Other than Workstation
 
 # Delete spot instances
@@ -20,13 +22,29 @@ for i in $SIDS ; do
 done
 
 # Delete the instances
-INSTANCES=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress,State:State.Name,Name:Tags[?Key==`Name`]|[0].Value}' --output table | grep i- | grep -v workstation | awk '{print $2}' |xargs)
+INSTANCES=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Instance:InstanceId,PublicIP:PublicIpAddress,PrivateIP:PrivateIpAddress,State:State.Name,Name:Tags[?Key==`Name`]|[0].Value}' --output table | grep i- | grep -Ev 'workstation|terminated' | awk '{print $2}' |xargs)
 for i in $INSTANCES; do
   aws ec2 terminate-instances --instance-ids $i
 done
 
 
 # Delete IAM Users
+
+USERS=$(aws iam list-users --query 'Users[*].UserName' --output text)
+for user in $USERS; do
+  policies=$(aws iam list-user-policies --user-name $user --query 'PolicyNames[*]' --output text)
+  for policy in ${policies}; do
+    aws iam detach-user-policy --user-name $user --policy-arn arn:aws:iam::${AccountNo}:policy/$policy
+    aws iam delete-user-policy --user-name $user --policy-name $policy
+  done
+
+  keys=$(aws iam list-access-keys     --user-name $user --query 'AccessKeyMetadata[*].AccessKeyId' --output text)
+  for key in $keys ; do
+    aws iam delete-access-key --access-key-id $key --user-name $user
+  done
+
+  aws iam delete-user --user-name $user
+done
 
 # List IAM Users
 CURRENT_USER=$(aws sts get-caller-identity | jq '.Arn' |sed -e 's/"//g')
@@ -36,9 +54,19 @@ for user in ${ALL_USERS}; do
   aws iam delete-user --user-name $user
 done
 
+# IAM ROLES
+
 ROLES=$(aws iam list-roles --query 'Roles[].RoleName' --output text|xargs -n1 | grep -Ev '^AWS|OrganizationAccountAccessRole|workstation')
 
+
 for role in ${ROLES}; do
+  aws iam remove-role-from-instance-profile --instance-profile-name $role --role-name $role
+  for policyarn in $(aws iam list-attached-role-policies --role-name $role --query 'AttachedPolicies[*].PolicyArn' --output text); do
+    aws iam detach-role-policy --role-name  $role --policy-arn $policyarn
+  done
+  for inline_policy in $(aws iam list-role-policies --role-name $role --query 'PolicyNames[*]' --output text); do
+    aws iam delete-role-policy --role-name $role --policy-name ${inline_policy}
+  done
   aws iam delete-role --role-name $role
 done
 
@@ -67,4 +95,47 @@ PARAMS=$(aws ssm describe-parameters --query 'Parameters[].Name' --output text |
 
 for param in $PARAMS; do
   aws ssm delete-parameter --name $param
+done
+
+
+## Route53
+zones=$(aws route53 list-hosted-zones --query "HostedZones[*].{ID:Id,Name:Name,Private:Config.PrivateZone}" --output text | awk -F / '{print $NF}')
+for zone in $zones ; do
+  records=$(aws route53 list-resource-record-sets --hosted-zone-id $zone --query 'ResourceRecordSets[*].Name' --output text)
+  for record in $records ; do
+
+echo '{
+  "Comment": "Created Server - Private IP address - IPADDRESS , DNS Record - COMPONENT-dev.DOMAIN_NAME",
+  "Changes": [{
+    "Action": "DELETE",
+    "ResourceRecordSet": {
+      "Name": "COMPONENT",
+      "Type": "A",
+      "TTL": 30,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+}' | sed -e "s/COMPONENT/$record/" -e "s/IPADDRESS/1.1.1.1/" >/tmp/record.json
+
+  aws route53 change-resource-record-sets --hosted-zone-id $zone --change-batch file:///tmp/record.json
+echo '{
+  "Comment": "Created Server - Private IP address - IPADDRESS , DNS Record - COMPONENT-dev.DOMAIN_NAME",
+  "Changes": [{
+    "Action": "DELETE",
+    "ResourceRecordSet": {
+      "Name": "COMPONENT",
+      "Type": "CNAME",
+      "TTL": 30,
+      "ResourceRecords": [{ "Value": "www.google.com"}]
+    }}]
+}' | sed -e "s/COMPONENT/$record/" >/tmp/record.json
+
+  aws route53 change-resource-record-sets --hosted-zone-id $zone --change-batch file:///tmp/record.json
+  done
+  aws route53 delete-hosted-zone --id $zone
+done
+
+## ACM
+certs=$(aws acm list-certificates --query 'CertificateSummaryList[*].CertificateArn' --output text)
+for cert in $certs ; do
+  aws acm delete-certificate --certificate-arn $cert
 done
