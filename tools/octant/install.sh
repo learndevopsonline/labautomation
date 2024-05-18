93e925d54099142cfd95ffd34345c8741dd7672b
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-18 07:24:44 +0530

    Minikube Changes

--- tools/octant/install.sh
+++ tools/octant/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-yum install https://github.com/vmware-archive/octant/releases/download/v0.25.1/octant_0.25.1_Linux-64bit.rpm
+yum install https://github.com/vmware-archive/octant/releases/download/v0.25.1/octant_0.25.1_Linux-64bit.rpm -y
 cp /tmp/labautomation/tools/octant/octant.service /etc/systemd/system/octant.service
 systemctl enable octant
 systemctl start octant
