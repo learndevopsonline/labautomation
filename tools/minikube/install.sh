b2a95989b86cf0a24cd053aa9a507bbadb278de6
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-18 06:43:52 +0530

    Minikube Changes

--- tools/minikube/install.sh
+++ tools/minikube/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo Hello World
+type minikube &>/dev/null
+if [ $? -ne 0 ]; then
+  dnf install https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+fi
+
