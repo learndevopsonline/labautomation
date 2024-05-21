c3cce17897a255692417c81e6a1f0e3759f80359
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-21 07:14:54 +0530

    docker install script

--- tools/docker/install.sh
+++ tools/docker/install.sh
@@ -1,21 +1,20 @@
 #!/bin/bash 
 
-if [ $(id -u) -ne 0 ]; then 
-  echo "Run as root user"
-  exit 1
-fi 
-
-#yum install -y yum-utils device-mapper-persistent-data lvm2
-#yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#yum install docker-ce docker-ce-cli containerd.io -y
-
-curl -s https://get.docker.com | bash
+rpm -qi redhat-release &>/dev/null
+if [ $? -eq 0 ]; then
+  user=ec2-user
+  yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+  yum install docker-ce --nobest -y
+else
+  curl -s https://get.docker.com | bash
+  user=centos
+fi
 
 ## Group add 
-id centos &>/dev/null
+id $user &>/dev/null
 if [ $? -eq 0 ]; then 
   groupadd docker &>/dev/null
-  usermod -a -G docker centos
+  usermod -a -G docker $user
 fi
 
 systemctl enable docker 
