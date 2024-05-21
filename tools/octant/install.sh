f64bfaeb24f039ae865e470dcfaac97ba305af81
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-21 07:20:55 +0530

    octant install script

--- tools/octant/install.sh
+++ tools/octant/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
 
 yum install https://github.com/vmware-archive/octant/releases/download/v0.25.1/octant_0.25.1_Linux-64bit.rpm -y
-cp /tmp/labautomation/tools/octant/octant.service /etc/systemd/system/octant.service
+rpm -qa | grep minikube &>/dev/null
+if [ $? -eq 0 ]; then
+  cp /tmp/labautomation/tools/octant/octant-minikube.service /etc/systemd/system/octant.service
+else
+  cp /tmp/labautomation/tools/octant/octant.service /etc/systemd/system/octant.service
+fi
+systemctl daemon-reload
 systemctl enable octant
 systemctl start octant
 
