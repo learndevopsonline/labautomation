ae5fbd5261918f2a37ec3690147eb663049d2180
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-27 06:43:04 +0530

    m

--- tools/argocd/install.sh
+++ tools/argocd/install.sh
@@ -1,3 +1,3 @@
-curl -sSL -o /tmp/argocd-linux-amd64 argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+curl -sSL -o /tmp/argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 install -m 555 /tmp/argocd-linux-amd64 /usr/local/bin/argocd
 rm /tmp/argocd-linux-amd64
