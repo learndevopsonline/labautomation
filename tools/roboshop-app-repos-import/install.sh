4dbb7bf36c5049e3455c45031e02df74336dcf09
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-01 06:50:57 +0530

    m

--- tools/roboshop-app-repos-import/install.sh
+++ tools/roboshop-app-repos-import/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+type gh &>/dev/null
+if [ $? -ne 0 ]; then
+  echo gh cli is missing.
+  echo -e "Run \e[1;33msudo labauto github-cli\e[0m to install it"
+  exit 1
+fi
+
 read -p 'Enter GitHub Username/OrganizationName: ' gusername
 
 SOURCE_REPO=https://github.com/roboshop-devops-project-v1
@@ -14,5 +21,5 @@ for component in cart catalogue user shipping payment dispatch frontend ; do
   git branch -m main
   git add -A
   git commit -m INIT
-  gh repo create $gusername/$component --public --source=. --push
+  gh repo create $gusername/roboshop-$component --public --source=. --push
 done
