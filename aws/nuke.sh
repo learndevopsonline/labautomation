ae68445fa3e53a7bdfeeffeca5e9713bd3ccc3d6
SVR Kalluru <raghuk.vit@gmail.com>
2024-06-17 12:22:18 +0530

    m

--- aws/nuke.sh
+++ aws/nuke.sh
@@ -167,7 +167,7 @@ echo '{
 }' | sed -e "s/COMPONENT/$name/" -e "s/TYPE/${type}/" -e "s|ZONE|${zone}|" >/tmp/record.json
 
 cat /tmp/record.json
-break
+exit
 
 aws route53  change-resource-record-sets --hosted-zone-id $zone --change-batch file:///tmp/record.json
 
