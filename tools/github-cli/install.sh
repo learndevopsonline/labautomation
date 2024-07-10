660141d4ebf63ef3aa04ade0b0f8b4cfb9299fc3
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-10 08:07:51 +0530

    RoboShop-V2

--- tools/github-cli/install.sh
+++ tools/github-cli/install.sh
@@ -5,6 +5,7 @@ if [ $? -ne 0 ]; then
   curl -L https://cli.github.com/packages/rpm/gh-cli.repo >/etc/yum.repos.d/gh.repo
   yum install gh -y
   gh auth login && gh repo list
+  gh auth refresh -h github.com -s admin:org
 fi
 
 
