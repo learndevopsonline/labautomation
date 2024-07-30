f2614ecf31a2019140e95f5c167482b06d8d9fbe
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-30 17:53:53 +0530

    m

--- tools/grizzly/install.sh
+++ tools/grizzly/install.sh
@@ -1,3 +1,3 @@
-URL=$(https://github.com/grafana/grizzly/releases/download/v0.4.3/grr-linux-amd64)
+URL=$(curl -s -L https://github.com/grafana/grizzly/releases | grep linux-amd64 | head -1 | awk -F '[&,;]' '{print $(NF-2)}')
 curl -L -o /bin/grr $URL
 chmod +x /bin/grr
