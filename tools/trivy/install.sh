14d7f273c2b18abfb7fd3eb9e00f6a5969e0b13d
Raghu K <raghuk.vit@gmail.com>
2024-10-17 06:48:53 +0530

    Change

---
+++ tools/trivy/install.sh
@@ -0,0 +1,9 @@
+cat << EOF | sudo tee -a /etc/yum.repos.d/trivy.repo
+[trivy]
+name=Trivy repository
+baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://aquasecurity.github.io/trivy-repo/rpm/public.key
+EOF
+sudo yum -y install trivy
