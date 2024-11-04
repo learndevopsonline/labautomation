891795d90c27f550592d2f4cb03a898584755b72
Raghu K <raghuk.vit@gmail.com>
2024-11-04 21:57:15 +0530

    Change

--- tools/terraform/install.sh
+++ tools/terraform/install.sh
@@ -7,15 +7,5 @@ fi
 
 source /tmp/labautomation/dry/common-functions.sh
 yum install -y yum-utils
-case $OSVENDOR in
-  CentOS)
-    yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-  ;;
-  "Amazon Linux")
-    yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-  ;;
-  *)
-    echo "OS not Supproted"
-    exit 1
-esac
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 yum -y install terraform
