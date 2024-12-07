73a64f2d6c47a0764518141b9ab139b56513f4d2
Raghu K <raghuk.vit@gmail.com>
2024-12-07 23:22:21 +0530

    Change

---
+++ tools/prometheus-exporters/mysql-exporter.sh
@@ -0,0 +1,12 @@
+latest_version=$(curl -s https://github.com/prometheus/mysqld_exporter/releases | grep '/releases/download/v' | sed -e 's|/| |g' | awk '{print $7}' | head -1 | sed -e 's/v//')
+
+cd /opt
+curl -s -L -O https://github.com/prometheus/mysqld_exporter/releases/download/v${latest_version}/mysqld_exporter-${latest_version}.linux-amd64.tar.gz
+download_file=mysqld_exporter-${latest_version}.linux-amd64.tar.gz
+
+## Remove existing exporter
+rm -rf mysqld_exporter
+tar -xf $download_file
+rm -f $download_file
+
+
