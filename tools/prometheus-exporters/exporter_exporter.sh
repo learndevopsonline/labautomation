98fecb927969ff61c19b4766ee5efccdd1ac09b3
Raghu K <raghuk.vit@gmail.com>
2024-12-07 23:55:00 +0530

    Change

--- tools/prometheus-exporters/exporter_exporter.sh
+++ tools/prometheus-exporters/exporter_exporter.sh
@@ -4,27 +4,16 @@ cd /opt
 curl -s -L -O https://github.com/QubitProducts/exporter_exporter/releases/download/v${latest_version}/exporter_exporter-${latest_version}.linux-amd64.tar.gz
 download_file=exporter_exporter-${latest_version}.linux-amd64.tar.gz
 dir_name=exporter_exporter-${latest_version}.linux-amd64
-exit
 
 ## Remove existing exporter
-rm -rf mysqld_exporter
+rm -rf exporter_exporter
 tar -xf $download_file
 rm -f $download_file
-mv $dir_name mysqld_exporter
+mv $dir_name exporter_exporter
 
-echo '[client]
-user = exporter
-password = exporter123
-host = localhost' >/opt/mysqld_exporter/cnf
+cp /tmp/labautomation/tools/prometheus-exporters/exporter_exporter.conf /opt/exporter_exporter/exporter_exporter.conf
 
-cp /tmp/labautomation/tools/prometheus-exporters/mysqld_exporter.service /etc/systemd/system/mysqld_exporter.service
-systemctl enable mysqld_exporter
-systemctl start mysqld_exporter
-
-
-echo "Login to MySQL and run these mysql commands"
-echo '------------------------------------------------------------------------------------------'
-echo "CREATE USER 'exporter'@'localhost' IDENTIFIED BY 'exporter' WITH MAX_USER_CONNECTIONS 3;"
-echo "GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'localhost';"
-echo '------------------------------------------------------------------------------------------'
+cp /tmp/labautomation/tools/prometheus-exporters/exporter_exporter.service /etc/systemd/system/exporter_exporter.service
+systemctl enable exporter_exporter
+systemctl start exporter_exporter
 
