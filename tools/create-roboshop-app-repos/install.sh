31e2855b581ba9ad4f8e837fc5e16d74476c36e6
Raghu K <raghuk.vit@gmail.com>
2025-06-09 07:06:36 +0530

    Change

--- tools/create-roboshop-app-repos/install.sh
+++ tools/create-roboshop-app-repos/install.sh
@@ -4,9 +4,11 @@ echo All the repos are coming from https://github.com/roboshop-devops-project-v1
 echo
 echo
 
-read -p 'Enter Username: ' username
+read -p 'ORG Name: ' username
 
 for repo in catalogue user cart shipping payment frontend dispatch ; do
+  gh repo list $username  | grep roboshop-$repo
+  if [ $? -eq 0 ]; then echo Repo Already Exists; continue ;fi
   cd /tmp
   git clone https://github.com/roboshop-devops-project-v1/$repo
   cd /home/centos
