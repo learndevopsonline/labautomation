a96c08886c72b64c656fa4f24bdb409a83ce5035
Raghu K <raghuk.vit@gmail.com>
2025-09-01 08:04:56 +0530

    Change

--- tools/prometheus-node-exporter/install.sh
+++ tools/prometheus-node-exporter/install.sh
@@ -11,7 +11,7 @@ if [ -d /opt/node_exporter ]; then
   exit 0
 fi 
 
-URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep node_exporter | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http | head -1)
+URL=$(curl -L -s https://prometheus.io/download/  | grep tar | grep prometheus- | grep linux-amd64  | sed -e "s|>| |g" -e 's|<| |g' -e 's|"| |g' |xargs -n1 | grep ^http | grep 'https://github.com/prometheus/node_exporter/releases/download/' | grep linux-amd64  | head -n1)
 
 FILENAME=$(echo $URL | awk -F / '{print $NF}')
 DIRNAME=$(echo $FILENAME | sed -e 's/.tar.gz//')
