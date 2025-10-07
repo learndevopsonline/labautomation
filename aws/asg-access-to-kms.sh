2ef5f50dc8aaa12ea2128c2828873646fecdc78a
Raghu K <raghuk.vit@gmail.com>
2025-10-07 08:31:27 +0530

    Change

--- aws/asg-access-to-kms.sh
+++ aws/asg-access-to-kms.sh
@@ -1,7 +1,7 @@
 echo -e "\e[1;33m Reference Document\e[0m : https://docs.aws.amazon.com/autoscaling/ec2/userguide/key-policy-requirements-EBS-encryption.html"
 
 ## Get the KMS ID
-KC=$(aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | wc -l)
+KC=$(/usr/local/bin/aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | wc -l)
 if [ $KC -gt 1 ]; then
   echo "Multiple KMS Keys found, Existing..."
   echo "Manually run the command from the above given URL"
@@ -13,8 +13,8 @@ if [ $KC -eq 0 ]; then
   exit 1
 fi
 
-ALIAS=$(aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | awk -F : '{print $NF}')
-ARN=$(aws kms describe-key --key-id $ALIAS --query 'KeyMetadata.Arn' --output text)
-ACCOUNT=$(aws sts get-caller-identity  --query 'Account' --output text)
+ALIAS=$(/usr/local/bin/aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | awk -F : '{print $NF}')
+ARN=$(/usr/local/bin/aws kms describe-key --key-id $ALIAS --query 'KeyMetadata.Arn' --output text)
+ACCOUNT=$(/usr/local/bin/aws sts get-caller-identity  --query 'Account' --output text)
 
-aws kms create-grant --region us-east-1 --key-id $ARN --grantee-principal arn:aws:iam::$ACCOUNT:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling --operations "Encrypt" "Decrypt" "ReEncryptFrom" "ReEncryptTo" "GenerateDataKey" "GenerateDataKeyWithoutPlaintext" "DescribeKey" "CreateGrant"
+/usr/local/bin/aws kms create-grant --region us-east-1 --key-id $ARN --grantee-principal arn:aws:iam::$ACCOUNT:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling --operations "Encrypt" "Decrypt" "ReEncryptFrom" "ReEncryptTo" "GenerateDataKey" "GenerateDataKeyWithoutPlaintext" "DescribeKey" "CreateGrant"
