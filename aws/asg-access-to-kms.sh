463a189912048251e43efc6cf13ec507ce089d23
Raghu K <raghuk.vit@gmail.com>
2025-10-07 08:42:26 +0530

    Change

--- aws/asg-access-to-kms.sh
+++ aws/asg-access-to-kms.sh
@@ -1,20 +1,25 @@
 echo -e "\e[1;33m Reference Document\e[0m : https://docs.aws.amazon.com/autoscaling/ec2/userguide/key-policy-requirements-EBS-encryption.html"
 
 ## Get the KMS ID
-KC=$(/usr/local/bin/aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | wc -l)
-if [ $KC -gt 1 ]; then
-  echo "Multiple KMS Keys found, Existing..."
+LIST=$(/usr/local/bin/aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn  | sed -e 's/"/ /g' | xargs -n1 | grep ^arn)
+for arn in $LIST ; do
+  state=(aws kms describe-key --key-id $i --query 'KeyMetadata.KeyState')
+  if [ "state" == "Enabled" ]; then
+    keys="$keys $arn"
+  fi
+done
+if [ "$(echo $keys | xargs -n1 |wc -l)" -gt 1 ]; then
+  echo "Multiple KMS Keys found, Exiting..."
   echo "Manually run the command from the above given URL"
   exit 1
 fi
 
-if [ $KC -eq 0 ]; then
+if [ "$(echo $keys | xargs -n1 |wc -l)" -eq 0 ]; then
   echo "No KMS Keys found, Existing..."
   exit 1
 fi
 
-ALIAS=$(/usr/local/bin/aws kms list-aliases | grep -E 'AliasName|AliasArn' | sed -e '/\/aws\// d' | grep AliasArn | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | awk -F : '{print $NF}')
-ARN=$(/usr/local/bin/aws kms describe-key --key-id $ALIAS --query 'KeyMetadata.Arn' --output text)
+ARN=$(/usr/local/bin/aws kms describe-key --key-id $keys --query 'KeyMetadata.Arn' --output text)
 ACCOUNT=$(/usr/local/bin/aws sts get-caller-identity  --query 'Account' --output text)
 
 /usr/local/bin/aws kms create-grant --region us-east-1 --key-id $ARN --grantee-principal arn:aws:iam::$ACCOUNT:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling --operations "Encrypt" "Decrypt" "ReEncryptFrom" "ReEncryptTo" "GenerateDataKey" "GenerateDataKeyWithoutPlaintext" "DescribeKey" "CreateGrant"
