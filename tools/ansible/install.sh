73084fc5d6a126be919afeaa0ef298b903ab9be3
Raghu K <raghuk.vit@gmail.com>
2025-11-27 07:16:52 +0530

    m

--- tools/ansible/install.sh
+++ tools/ansible/install.sh
@@ -10,6 +10,6 @@ elif [ "$ELV" == "el8" ]; then
   yum install python3.12-devel python3.12-pip -y
   pip3.12 install ansible ansible-core==2.16 botocore boto3 python-jenkins
 elif [ "$ELV" == "el9" ]; then
-  yum install python3.11-devel python3.11-pip -y
-  pip3.11 install ansible botocore boto3 python-jenkins hvac
+  yum install python3.13-devel python3.13-pip -y
+  pip3.13 install ansible
 fi
